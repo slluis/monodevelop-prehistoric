65056441130aacfc455417ed5088f88c84e3127d
jluke <jluke@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2004-01-27 01:28:54 +0000

    forgot to add this
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@666 b5faff8f-5bce-0310-b493-a43f95cdeebf

---
+++ MonoDevelop/src/Libraries/MonoDevelop.Gui/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("MonoDevelop.Gui")]
+[assembly: AssemblyDescription("A .NET text editor control")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("MonoDevelop.Gui")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
