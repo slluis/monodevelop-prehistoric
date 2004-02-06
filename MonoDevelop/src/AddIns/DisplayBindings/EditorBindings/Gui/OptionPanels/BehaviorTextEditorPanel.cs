53aa9efc83621c5d47ec4256b249bb677c5169cc
dkor <dkor@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2004-02-06 07:09:42 +0000

    Glading of the BehaviorTextEditorPanel
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@802 b5faff8f-5bce-0310-b493-a43f95cdeebf

--- MonoDevelop/src/AddIns/DisplayBindings/EditorBindings/Gui/OptionPanels/BehaviorTextEditorPanel.cs
+++ MonoDevelop/src/AddIns/DisplayBindings/EditorBindings/Gui/OptionPanels/BehaviorTextEditorPanel.cs
@@ -21,6 +21,7 @@ using ICSharpCode.TextEditor.Document;
 using ICSharpCode.SharpDevelop.Gui.Dialogs;
 
 using Gtk;
+using MonoDevelop.Gui.Widgets;
 using MonoDevelop.EditorBindings.FormattingStrategy;
 
 namespace MonoDevelop.EditorBindings.Gui.OptionPanels
@@ -30,146 +31,126 @@ namespace MonoDevelop.EditorBindings.Gui.OptionPanels
 	/// </summary>
 	public class BehaviorTextEditorPanel : AbstractOptionPanel
 	{
-		// GTK controls
-		CheckButton		autoinsertCurlyBraceCheckBox;
-		CheckButton		hideMouseCursorCheckBox;
-		CheckButton		caretBehindEOLCheckBox;
-		CheckButton		auotInsertTemplatesCheckBox;
-		CheckButton		convertTabsToSpacesCheckBox;
-		Entry			tabSizeTextBox;
-		Entry			indentSizeTextBox;
-		Gtk.Menu		indentStyleComboBoxMenu;
-		Gtk.OptionMenu	indentStyleComboBox;
-		Gtk.Menu		mouseWhellDirectionComboBoxMenu;
-		Gtk.OptionMenu	mouseWhellDirectionComboBox;
-		
-		// Services
-		StringParserService StringParserService = (StringParserService)ServiceManager.Services.GetService (typeof (StringParserService));
+		BehaviorTextEditorPanelWidget widget;
 		
 		public override void LoadPanelContents()
 		{
-			// set up the form controls instance
-			SetupPanelInstance();			
-			
-			autoinsertCurlyBraceCheckBox.Active = ((IProperties)CustomizationObject).GetProperty("AutoInsertCurlyBracket", true);
-			hideMouseCursorCheckBox.Active      = ((IProperties)CustomizationObject).GetProperty("HideMouseCursor", true);
-			caretBehindEOLCheckBox.Active       = ((IProperties)CustomizationObject).GetProperty("CursorBehindEOL", false);
-			auotInsertTemplatesCheckBox.Active  = ((IProperties)CustomizationObject).GetProperty("AutoInsertTemplates", true);
-			
-			convertTabsToSpacesCheckBox.Active  = ((IProperties)CustomizationObject).GetProperty("TabsToSpaces", false);
-			
-			tabSizeTextBox.Text    = ((IProperties)CustomizationObject).GetProperty("TabIndent", 4).ToString();
-			indentSizeTextBox.Text = ((IProperties)CustomizationObject).GetProperty("IndentationSize", 4).ToString();
-			
-			indentStyleComboBoxMenu.Append(MenuItem.NewWithLabel(StringParserService.Parse("${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.IndentStyle.None}")));
-			indentStyleComboBoxMenu.Append(MenuItem.NewWithLabel(StringParserService.Parse("${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.IndentStyle.Automatic}")));
-			indentStyleComboBoxMenu.Append(MenuItem.NewWithLabel(StringParserService.Parse("${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.IndentStyle.Smart}")));
-			
-			indentStyleComboBox.SetHistory((uint)(IndentStyle)((IProperties)CustomizationObject).GetProperty("IndentStyle", IndentStyle.Smart));
-		
-			mouseWhellDirectionComboBoxMenu.Append(MenuItem.NewWithLabel(StringParserService.Parse("${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.NormalMouseDirectionRadioButton}")));
-			mouseWhellDirectionComboBoxMenu.Append(MenuItem.NewWithLabel(StringParserService.Parse("${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.ReverseMouseDirectionRadioButton}")));
-			int selectedIndex = ((IProperties)CustomizationObject).GetProperty("MouseWheelScrollDown", true) ? 0 : 1;
-			mouseWhellDirectionComboBox.SetHistory((uint)selectedIndex);
+			Add (widget = new BehaviorTextEditorPanelWidget ((IProperties) CustomizationObject));
 		}
 		
 		public override bool StorePanelContents()
 		{
-			((IProperties)CustomizationObject).SetProperty("TabsToSpaces",         convertTabsToSpacesCheckBox.Active);
-			((IProperties)CustomizationObject).SetProperty("MouseWheelScrollDown", mouseWhellDirectionComboBox.History == 0);
-			
-			((IProperties)CustomizationObject).SetProperty("AutoInsertCurlyBracket", autoinsertCurlyBraceCheckBox.Active);
-			((IProperties)CustomizationObject).SetProperty("HideMouseCursor",        hideMouseCursorCheckBox.Active);
-			((IProperties)CustomizationObject).SetProperty("CursorBehindEOL",        caretBehindEOLCheckBox.Active);
-			((IProperties)CustomizationObject).SetProperty("AutoInsertTemplates",    auotInsertTemplatesCheckBox.Active);
-			
-			((IProperties)CustomizationObject).SetProperty("IndentStyle", indentStyleComboBox.History);
-			
-			try {
-				int tabSize = Int32.Parse(tabSizeTextBox.Text);
-				
-				// FIX: don't allow to set tab size to zero as this will cause divide by zero exceptions in the text control.
-				// Zero isn't a setting that makes sense, anyway.
-				if (tabSize > 0) {
-					((IProperties)CustomizationObject).SetProperty("TabIndent", tabSize);
-				}
-			} catch (Exception) {
-			}
-			
-			try {
-				((IProperties)CustomizationObject).SetProperty("IndentationSize", Int32.Parse(indentSizeTextBox.Text));
-			} catch (Exception) {
-			}
+			widget.Store ((IProperties) CustomizationObject);
 			return true;
 		}
 		
-		#region jba added methods
-		
-		private void SetupPanelInstance()
+		class BehaviorTextEditorPanelWidget : GladeWidgetExtract 
 		{
-			Gtk.Frame frame1 = new Gtk.Frame(StringParserService.Parse("${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.TabsGroupBox}"));
-			//
-			// set up the Tab
-			//
-			// instantiate all the controls
-			Gtk.VBox vBox1 = new Gtk.VBox(false,2);
-			Gtk.Label label1 = new Gtk.Label(StringParserService.Parse("${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.TabSizeLabel}"));
-			Gtk.Label label2 = new Gtk.Label(StringParserService.Parse("${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.IndentSizeLabel}"));
-			Gtk.Label label3 = new Gtk.Label(StringParserService.Parse("${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.IndentLabel}"));
-			tabSizeTextBox = new Entry();
-			indentSizeTextBox = new Entry();
-			indentStyleComboBoxMenu = new Menu();
-			indentStyleComboBox = new OptionMenu();
-			indentStyleComboBox.Menu = indentStyleComboBoxMenu;
-			convertTabsToSpacesCheckBox = new CheckButton(StringParserService.Parse("${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.ConvertTabsToSpacesCheckBox}"));
-				// table to pack the tab settings options 
-				Table table1 = new Table(4, 2, false);
-				//pack the table
-				table1.Attach(label1, 0, 1, 0, 1);
-				table1.Attach(tabSizeTextBox, 1, 2, 0, 1);
-				table1.Attach(label2, 0, 1, 1, 2);
-				table1.Attach(indentSizeTextBox, 1, 2, 1, 2);
-				table1.Attach(label3, 2, 4, 0, 1);
-				table1.Attach(indentStyleComboBox, 2, 4, 1, 2);
+			// Services
+			StringParserService StringParserService = (
+				StringParserService)ServiceManager.Services.GetService (typeof (StringParserService));
+			
+			// GTK controls
+			[Glade.Widget] Label			tabsGroupBoxLabel;
+			[Glade.Widget] Label			behaviourGroupBoxLabel;
+			[Glade.Widget] Label			tabSizeLabel;
+			[Glade.Widget] Label			indentSizeLabel;
+			[Glade.Widget] Label			indentLabel;
+			[Glade.Widget] CheckButton		autoinsertCurlyBraceCheckBox;
+			[Glade.Widget] CheckButton		hideMouseCursorCheckBox;
+			[Glade.Widget] CheckButton		caretBehindEOLCheckBox;
+			[Glade.Widget] CheckButton		autoInsertTemplatesCheckBox;
+			[Glade.Widget] CheckButton		convertTabsToSpacesCheckBox;
+			[Glade.Widget] RadioButton              noneIndentStyle;
+			[Glade.Widget] RadioButton              automaticIndentStyle;
+			[Glade.Widget] RadioButton              smartIndentStyle;
+			[Glade.Widget] SpinButton               indentAndTabSizeSpinButton;
+			
+			public BehaviorTextEditorPanelWidget (IProperties CustomizationObject) :  
+				base ("EditorBindings.glade", "BehaviorTextEditorPanel")
+			{
+				// Set up Text
+				tabsGroupBoxLabel.Markup = "<b>" + StringParserService.Parse(
+					"${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.TabsGroupBox}")  + "</b>";
+				tabSizeLabel.TextWithMnemonic = StringParserService.Parse(
+					"${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.TabSizeLabel}");
+				indentSizeLabel.TextWithMnemonic = StringParserService.Parse(
+					"${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.IndentSizeLabel}");
+				indentLabel.TextWithMnemonic = StringParserService.Parse(
+					"${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.IndentLabel}");
 				
-			// pack them all
-			vBox1.PackStart(table1, false, false, 2);
-			vBox1.PackStart(convertTabsToSpacesCheckBox, false, false, 2);
-			frame1.Add(vBox1);
-			
-			//
-			// setup the behaviour options
-			//
-			//instantiate all the controls			
-			Gtk.Frame frame2 = new Gtk.Frame(StringParserService.Parse("${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.BehaviourGroupBox}"));
-			Gtk.VBox vBox2 = new Gtk.VBox(false, 2);
-			
-			
-			autoinsertCurlyBraceCheckBox = new CheckButton(StringParserService.Parse("${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.CurlyBracketCheckBox}"));
-			hideMouseCursorCheckBox  = new CheckButton(StringParserService.Parse("${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.HideMouseCheckBox}"));
-			caretBehindEOLCheckBox  = new CheckButton(StringParserService.Parse("${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.BehindEOLCheckBox}"));
-			auotInsertTemplatesCheckBox  = new CheckButton(StringParserService.Parse("${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.TemplateInsertCheckBox}"));			
-			Label label4 = new Gtk.Label(StringParserService.Parse("${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.MouseWhellGroupBox}"));
-			mouseWhellDirectionComboBoxMenu = new Menu();
-			mouseWhellDirectionComboBox = new OptionMenu();
-			mouseWhellDirectionComboBox.Menu = mouseWhellDirectionComboBoxMenu;
-			// pack them all
-			vBox2.PackStart(caretBehindEOLCheckBox, false, false, 2);
-			vBox2.PackStart(auotInsertTemplatesCheckBox, false, false, 2);
-			vBox2.PackStart(autoinsertCurlyBraceCheckBox, false, false, 2);
-			vBox2.PackStart(hideMouseCursorCheckBox, false, false, 2);
-			vBox2.PackStart(label4, false, false, 2);
-			vBox2.PackStart(mouseWhellDirectionComboBox, false, false, 2);
-			frame2.Add(vBox2);
-			
-			// create the main box
-			Gtk.VBox mainBox = new Gtk.VBox(false, 2);
-			mainBox.PackStart(frame1, false, false, 2);
-			mainBox.PackStart(frame2, false, false, 2);
-			
-			this.Add(mainBox);
+				behaviourGroupBoxLabel.Markup = "<b>" +  StringParserService.Parse(
+					"${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.BehaviourGroupBox}") + "</b>";
+				autoinsertCurlyBraceCheckBox.Label = StringParserService.Parse(
+					"${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.CurlyBracketCheckBox}");
+				hideMouseCursorCheckBox.Label = StringParserService.Parse(
+					"${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.HideMouseCheckBox}");
+				caretBehindEOLCheckBox.Label = StringParserService.Parse(
+					"${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.BehindEOLCheckBox}");
+				autoInsertTemplatesCheckBox.Label = StringParserService.Parse(
+					"${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.TemplateInsertCheckBox}");
+				convertTabsToSpacesCheckBox.Label = StringParserService.Parse(
+					"${res:Dialog.Options.IDEOptions.TextEditor.Behaviour.ConvertTabsToSpacesCheckBox}");			
+				
+				// Set up Value
+				autoinsertCurlyBraceCheckBox.Active = ((IProperties)CustomizationObject).GetProperty(
+					"AutoInsertCurlyBracket", true);
+				hideMouseCursorCheckBox.Active      = ((IProperties)CustomizationObject).GetProperty(
+					"HideMouseCursor", true);
+				caretBehindEOLCheckBox.Active       = ((IProperties)CustomizationObject).GetProperty(
+					"CursorBehindEOL", false);
+				autoInsertTemplatesCheckBox.Active  = ((IProperties)CustomizationObject).GetProperty(
+					"AutoInsertTemplates", true);
+				convertTabsToSpacesCheckBox.Active  = ((IProperties)CustomizationObject).GetProperty(
+					"TabsToSpaces", false);
+				
+				//FIXME: Only one of these should be selected to hold the value
+				indentAndTabSizeSpinButton.Value  = ((IProperties)CustomizationObject).GetProperty(
+					"TabIndent", 4);
+				indentAndTabSizeSpinButton.Value = ((IProperties)CustomizationObject).GetProperty(
+					"IndentationSize", 4);
+
+				if (IndentStyle.None.Equals(
+					    (IndentStyle) ((IProperties)CustomizationObject).GetProperty(
+						    "IndentStyle", IndentStyle.Smart))){
+					noneIndentStyle.Active = true;
+				}
+				else if (IndentStyle.Auto.Equals(
+						 (IndentStyle) ((IProperties)CustomizationObject).GetProperty(
+							 "IndentStyle", IndentStyle.Smart))){
+					automaticIndentStyle.Active = true;
+				}
+				else if (IndentStyle.Smart.Equals(
+						 (IndentStyle) ((IProperties)CustomizationObject).GetProperty(
+							 "IndentStyle", IndentStyle.Smart))){
+					smartIndentStyle.Active = true;
+				}
+			}
+
+			public void Store (IProperties CustomizationObject)
+			{
+				((IProperties)CustomizationObject).SetProperty(
+					"TabsToSpaces",           convertTabsToSpacesCheckBox.Active);
+				((IProperties)CustomizationObject).SetProperty(
+					"AutoInsertCurlyBracket", autoinsertCurlyBraceCheckBox.Active);
+				((IProperties)CustomizationObject).SetProperty(
+					"HideMouseCursor",        hideMouseCursorCheckBox.Active);
+				((IProperties)CustomizationObject).SetProperty(
+					"CursorBehindEOL",        caretBehindEOLCheckBox.Active);
+				((IProperties)CustomizationObject).SetProperty(
+					"AutoInsertTemplates",    autoInsertTemplatesCheckBox.Active);
+
+				if (noneIndentStyle.Active)
+					((IProperties)CustomizationObject).SetProperty("IndentStyle", IndentStyle.None);
+				else if (automaticIndentStyle.Active)
+					((IProperties)CustomizationObject).SetProperty("IndentStyle", IndentStyle.Auto);
+				else if (smartIndentStyle.Active)
+					((IProperties)CustomizationObject).SetProperty("IndentStyle", IndentStyle.Smart);
+				
+				//FIXME: Only one of these should be selected to save the value
+				((IProperties)CustomizationObject).SetProperty("TabIndent", indentAndTabSizeSpinButton.Value);
+				((IProperties)CustomizationObject).SetProperty("IndentationSize", indentAndTabSizeSpinButton.Value);
+			}
 		}
-		
-		#endregion
 	}
 }
