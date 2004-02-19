043aa3946560cd4b59a23a12d2c4d16b8fe0af53
dkor <dkor@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2004-02-19 02:32:39 +0000

    Ported and Gladed CompileFileProjectOptionsPanel
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@941 b5faff8f-5bce-0310-b493-a43f95cdeebf

--- MonoDevelop/src/Main/Base/Gui/Dialogs/OptionPanels/ProjectOptions/CompileFileProjectOptions.cs
+++ MonoDevelop/src/Main/Base/Gui/Dialogs/OptionPanels/ProjectOptions/CompileFileProjectOptions.cs
@@ -16,47 +16,113 @@ using ICSharpCode.Core.AddIns.Codons;
 using ICSharpCode.SharpDevelop.Internal.Project;
 using ICSharpCode.Core.Services;
 using ICSharpCode.Core.Properties;
+using ICSharpCode.SharpDevelop.Gui.Components;
+
+using Gtk;
+using MonoDevelop.Gui.Widgets;
 
 namespace ICSharpCode.SharpDevelop.Gui.Dialogs.OptionPanels
-{/*
+{
 	/// <summary>
 	/// Summary description for Form1.
 	/// </summary>
 	public class CompileFileProjectOptions : AbstractOptionPanel
 	{
-		IProject project;
+
+	// FIXME 
+	// - internationalize 
+ 	//		SetupFromXml(Path.Combine(PropertyService.DataDirectory, 
+ 	//		                          @"resources\panels\CompileFileProjectOptions.xfrm"));
+
+		class CompileFileOptionsWidget : GladeWidgetExtract 
+		{
+			// Gtk Controls
+			[Glade.Widget] Gtk.TreeView includeTreeView;
+			public ListStore store;
+			
+			IProject project;
+                       FileUtilityService fileUtilityService = (FileUtilityService)ServiceManager.Services.GetService(typeof(FileUtilityService));
+
+			public CompileFileOptionsWidget (IProperties CustomizationObject) : 
+				base ("Base.glade", "CompileFileOptionsPanel")
+			{
+				this.project = (IProject)((IProperties)CustomizationObject).GetProperty("Project");	
+				
+				store = new ListStore (typeof(bool), typeof(string));
+				includeTreeView.Selection.Mode = SelectionMode.None;
+				includeTreeView.Model = store;
+				CellRendererToggle rendererToggle = new CellRendererToggle ();
+				rendererToggle.Activatable = true;
+				rendererToggle.Toggled += new ToggledHandler (ItemToggled);
+				includeTreeView.AppendColumn ("Choosen", rendererToggle, "active", 0);
+				includeTreeView.AppendColumn ("Name", new CellRendererText (), "text", 1);
+				TreeIter iter = new TreeIter ();
+				
+				foreach (ProjectFile info in project.ProjectFiles) {
+					if (info.BuildAction == BuildAction.Nothing || info.BuildAction == BuildAction.Compile) {
+						string name = fileUtilityService.AbsoluteToRelativePath(
+							project.BaseDirectory, info.Name).Substring(2);
+						iter = store.AppendValues (info.BuildAction == BuildAction.Compile ? true : false, name);
+					}
+				}
+			}			
+			
+			private void ItemToggled (object o, ToggledArgs args)
+			{
+ 				const int column = 0;
+ 				Gtk.TreeIter iter;
+				
+				if (store.GetIterFromString(out iter, args.Path))
+ 				{
+ 					bool val = (bool) store.GetValue(iter, column);
+ 					store.SetValue(iter, column, !val);
+ 				}
+			}
+
+			public bool Store ()
+			{	
+				bool success = true;
+				TreeIter first;	
+				store.GetIterFirst(out first);
+				TreeIter current = first;
+				for (int i = 0; i < store.IterNChildren() - 1 ; ++i) {
+					store.IterNext(out current);
+					string name = fileUtilityService.RelativeToAbsolutePath(
+						project.BaseDirectory, "." + System.IO.Path.DirectorySeparatorChar + store.GetValue(current, 1));
+					int j = 0;
+					while (j < project.ProjectFiles.Count && project.ProjectFiles[j].Name != name) {
+						++j;
+					}
+					if (j < project.ProjectFiles.Count) {
+						project.ProjectFiles[j].BuildAction = (bool) store.GetValue(current, 0) ? BuildAction.Compile : BuildAction.Nothing;
+					} else {
+						string message = "File " + name + " not found in " + project.Name;
+						MessageDialog dialog = new MessageDialog ((Window) WorkbenchSingleton.Workbench, 
+								DialogFlags.DestroyWithParent,
+								MessageType.Error, 
+								ButtonsType.Close, 
+								message);	
+						dialog.Run ();
+						dialog.Hide ();
+						dialog.Dispose ();
+						success = false;
+					}
+				}
+				return true;
+			}
+		}
+		
+		CompileFileOptionsWidget widget;
 		
 		public override void LoadPanelContents()
 		{
-			SetupFromXml(Path.Combine(PropertyService.DataDirectory, 
-			                          @"resources\panels\CompileFileProjectOptions.xfrm"));
-			
-			this.project = (IProject)((IProperties)CustomizationObject).GetProperty("Project");
-			FileUtilityService fileUtilityService = (FileUtilityService)ServiceManager.Services.GetService(typeof(FileUtilityService));
-			foreach (ProjectFile info in project.ProjectFiles) {
-				if (info.BuildAction == BuildAction.Nothing || info.BuildAction == BuildAction.Compile) {
-					((CheckedListBox)ControlDictionary["IncludeFilesCheckedListBox"]).Items.Add(fileUtilityService.AbsoluteToRelativePath(project.BaseDirectory, info.Name).Substring(2), info.BuildAction == BuildAction.Compile ? CheckState.Checked : CheckState.Unchecked);
-				}
-			}
+			Add (widget = new  CompileFileOptionsWidget ((IProperties) CustomizationObject));
 		}
 		
 		public override bool StorePanelContents()
 		{
-			FileUtilityService fileUtilityService = (FileUtilityService)ServiceManager.Services.GetService(typeof(FileUtilityService));
-			for (int i = 0; i < ((CheckedListBox)ControlDictionary["IncludeFilesCheckedListBox"]).Items.Count; ++i) {
-				string name = fileUtilityService.RelativeToAbsolutePath(project.BaseDirectory, "." + Path.DirectorySeparatorChar + ((CheckedListBox)ControlDictionary["IncludeFilesCheckedListBox"]).Items[i].ToString());
-				int j = 0;
-				while (j < project.ProjectFiles.Count && project.ProjectFiles[j].Name != name) {
-					++j;
-				}
-				if (j < project.ProjectFiles.Count) {
-					project.ProjectFiles[j].BuildAction = ((CheckedListBox)ControlDictionary["IncludeFilesCheckedListBox"]).GetItemChecked(i) ? BuildAction.Compile : BuildAction.Nothing;
-				} else {
-					MessageService.ShowError("File " + name + " not found in " + project.Name);
-				}
-			}
-			
-			return true;
-		}
-	}*/
+			bool success = widget.Store();
+ 			return success;
+ 		}
+	}
 }
