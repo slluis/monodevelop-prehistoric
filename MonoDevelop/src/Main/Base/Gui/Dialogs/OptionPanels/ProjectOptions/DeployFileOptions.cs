43eaa416bdabc04c16da74225997abc65ed46bcf
dkor <dkor@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2004-02-22 06:21:58 +0000

    Ported and Gladed DeployFileOptionsPanel
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@984 b5faff8f-5bce-0310-b493-a43f95cdeebf

--- MonoDevelop/src/Main/Base/Gui/Dialogs/OptionPanels/ProjectOptions/DeployFileOptions.cs
+++ MonoDevelop/src/Main/Base/Gui/Dialogs/OptionPanels/ProjectOptions/DeployFileOptions.cs
@@ -13,116 +13,205 @@ using System.ComponentModel;
 
 using ICSharpCode.SharpDevelop.Internal.Project;
 using ICSharpCode.Core.Properties;
-
 using ICSharpCode.Core.Services;
 using ICSharpCode.Core.AddIns.Codons;
+using ICSharpCode.SharpDevelop.Gui.Components;
+
+using Gtk;
+using MonoDevelop.Gui.Widgets;
 
 namespace ICSharpCode.SharpDevelop.Gui.Dialogs.OptionPanels
-{/*
+{
 	/// <summary>
 	/// Summary description for Form3.
 	/// </summary>
 	public class DeployFileProjectOptions : AbstractOptionPanel
 	{
-		IProject project;
+
+		// FIXME 
+		// - internationalize 
+		// 	   SetupFromXml(Path.Combine(PropertyService.DataDirectory, 
+		// 			                          @"resources\panels\DeployFileOptions.xfrm"));
+
+		class DeployFileOptionsWidget : GladeWidgetExtract 
+		{
+		        // Gtk Controls
+ 			[Glade.Widget] RadioButton projectFileRadioButton;
+ 			[Glade.Widget] RadioButton compiledAssemblyRadioButton;
+ 			[Glade.Widget] RadioButton scriptFileRadioButton;
+ 			[Glade.Widget] Button selectScriptFileButton;
+ 			[Glade.Widget] Button selectTargetButton;
+ 			[Glade.Widget] Entry deployTargetEntry;
+ 			[Glade.Widget] Entry deployScriptEntry;
+			[Glade.Widget] VBox deployScriptBox;
+			[Glade.Widget] VBox deployTargetBox;
+			[Glade.Widget] Gtk.TreeView includeTreeView;
+			public ListStore store;
+
+			// Services
+			IProject project;
+			static FileUtilityService fileUtilityService = (FileUtilityService) ServiceManager.Services.GetService(typeof(FileUtilityService));
+			StringParserService StringParserService = (StringParserService)ServiceManager.Services.GetService (typeof (StringParserService));
+			public DeployFileOptionsWidget (IProperties CustomizationObject) : 
+				base ("Base.glade", "DeployFileOptionsPanel")
+			{
+				this.project = (IProject)((IProperties)CustomizationObject).GetProperty("Project");
+
+  				projectFileRadioButton.Clicked += new EventHandler(RadioButtonClicked);
+  				compiledAssemblyRadioButton.Clicked += new EventHandler(RadioButtonClicked);
+  				scriptFileRadioButton.Clicked += new EventHandler(RadioButtonClicked);
+				selectScriptFileButton.Clicked += new EventHandler(SelectScriptFileEvent);
+				selectTargetButton.Clicked += new EventHandler(SelectTargetFolderEvent);
+
+				store = new ListStore (typeof(bool), typeof(string));
+				includeTreeView.Selection.Mode = SelectionMode.None;
+				includeTreeView.Model = store;
+				CellRendererToggle rendererToggle = new CellRendererToggle ();
+				rendererToggle.Activatable = true;
+				rendererToggle.Toggled += new ToggledHandler (ItemToggled);
+				includeTreeView.AppendColumn ("Choosen", rendererToggle, "active", 0);
+				includeTreeView.AppendColumn ("Name", new CellRendererText (), "text", 1);
+				TreeIter iter = new TreeIter ();
+				
+				foreach (ProjectFile info in project.ProjectFiles) {
+					if (info.BuildAction != BuildAction.Exclude) {
+						string name = fileUtilityService.AbsoluteToRelativePath(project.BaseDirectory, info.Name);
+						iter = store.AppendValues (
+							project.DeployInformation.IsFileExcluded(info.Name) ? true : false, name);
+					}
+				}
+
+				deployTargetEntry.Text = project.DeployInformation.DeployTarget;
+				deployScriptEntry.Text = project.DeployInformation.DeployScript;
+			
+				projectFileRadioButton.Active = project.DeployInformation.DeploymentStrategy == DeploymentStrategy.File;
+				compiledAssemblyRadioButton.Active = project.DeployInformation.DeploymentStrategy == DeploymentStrategy.Assembly;
+				scriptFileRadioButton.Active = project.DeployInformation.DeploymentStrategy == DeploymentStrategy.Script;
+				
+				RadioButtonClicked(null, null);
+			}
+
+			//FIXME : Finish details in this dialog.
+			void SelectScriptFileEvent(object sender, EventArgs e)
+			{
+				FileSelection fs = new FileSelection ("Select your File"); // Put correct title 
+				fs.Complete("*.txt");
+//				 fs.Complete(StringParserService.Parse("${res:SharpDevelop.FileFilter.ExecutableFiles}|*.exe;*.com;*.pif;*.bat;*.cmd|${res:SharpDevelop.FileFilter.AllFiles}|*.*"));
+// 				fdiag.Filter = StringParserService.Parse("${res:SharpDevelop.FileFilter.ExecutableFiles}|*.exe;*.com;*.pif;*.bat;*.cmd|${res:SharpDevelop.FileFilter.AllFiles}|*.*");
+				if ( fs.Run () == (int) ResponseType.Ok) {
+ 					deployScriptEntry.Text = fs.SelectionEntry.Text;
+				}
+				fs.Hide (); 
+			}
+			
+			void SelectTargetFolderEvent(object sender, EventArgs e)
+			{
+				FileSelection fs = new FileSelection ("${res:Dialog.Options.PrjOptions.DeployFile.FolderDialogDescription}"); 
+				if ( fs.Run () == (int) ResponseType.Ok) {
+					deployTargetEntry.Text = fs.SelectionEntry.Text;
+				}
+				fs.Hide (); 
+			}
+			
+			void RadioButtonClicked(object sender, EventArgs e)
+			{
+ 				deployTargetBox.Sensitive = compiledAssemblyRadioButton.Active || projectFileRadioButton.Active;
+  				deployScriptEntry.Sensitive = scriptFileRadioButton.Active;
+  				selectScriptFileButton.Sensitive = scriptFileRadioButton.Active;
+			}
+			
+			private void ItemToggled (object o, ToggledArgs args)
+			{
+ 				const int column = 0;
+ 				Gtk.TreeIter iter;
+				
+				if (store.GetIterFromString(out iter, args.Path)) {
+ 					bool val = (bool) store.GetValue(iter, column);
+ 					store.SetValue(iter, column, !val);
+ 				}
+			}
+			
+			public bool Store () 
+			{
+				if (deployTargetEntry.Text.Length > 0) {
+					if (!fileUtilityService.IsValidFileName(deployTargetEntry.Text)) {
+						MessageDialog dialog = new MessageDialog ((Window) WorkbenchSingleton.Workbench, 
+								DialogFlags.DestroyWithParent,
+								MessageType.Error, 
+								ButtonsType.Close, 
+								"Invalid deploy target specified");
+						dialog.Run ();
+						dialog.Hide ();
+						dialog.Dispose ();
+						return false;
+					}
+				}
+				
+				if (deployScriptEntry.Text.Length > 0) {
+					if (!fileUtilityService.IsValidFileName(deployScriptEntry.Text)) {
+						MessageDialog dialog = new MessageDialog ((Window) WorkbenchSingleton.Workbench, 
+								DialogFlags.DestroyWithParent,
+								MessageType.Error, 
+								ButtonsType.Close, 
+								"Invalid deploy script specified");
+						dialog.Run ();
+						dialog.Hide ();
+						dialog.Dispose ();
+						return false;				
+					}
+				}
+				
+				if (!File.Exists(deployScriptEntry.Text)) {
+					MessageDialog dialog = new MessageDialog ((Window) WorkbenchSingleton.Workbench, 
+							DialogFlags.DestroyWithParent,
+							MessageType.Error, 
+							ButtonsType.Close, 
+							"Deploy script doesn't exists");
+					dialog.Run ();
+					dialog.Hide ();
+					dialog.Dispose ();
+					return false;
+ 				}
+			
+ 			project.DeployInformation.DeployTarget = deployTargetEntry.Text;
+ 			project.DeployInformation.DeployScript = deployScriptEntry.Text;
+			
+ 			if (projectFileRadioButton.Active) {
+ 				project.DeployInformation.DeploymentStrategy = DeploymentStrategy.File;
+ 			} else if (compiledAssemblyRadioButton.Active) {
+ 				project.DeployInformation.DeploymentStrategy = DeploymentStrategy.Assembly;
+ 			} else {
+ 				project.DeployInformation.DeploymentStrategy = DeploymentStrategy.Script;
+ 			}
+
+			TreeIter first;	
+			store.GetIterFirst(out first);
+			TreeIter current = first;
+ 			project.DeployInformation.ClearExcludedFiles();
+			for (int i = 0; i < store.IterNChildren() ; ++i) {
+				if ( (bool) store.GetValue(current, 0)){
+					project.DeployInformation.AddExcludedFile(fileUtilityService.RelativeToAbsolutePath(
+											  project.BaseDirectory, 
+											  (string) store.GetValue(current, 1)));
+							}
+				store.IterNext(out current);
+			}
+			return true;
+			}
+		}
+		
+		DeployFileOptionsWidget widget;
 		ResourceService resourceService = (ResourceService)ServiceManager.Services.GetService(typeof(IResourceService));
-		static FileUtilityService fileUtilityService = (FileUtilityService)ServiceManager.Services.GetService(typeof(FileUtilityService));
 		
 		public override void LoadPanelContents()
 		{
-			SetupFromXml(Path.Combine(PropertyService.DataDirectory, 
-			                          @"resources\panels\DeployFileOptions.xfrm"));
-			
-			((RadioButton)ControlDictionary["projectFileRadioButton"]).CheckedChanged += new EventHandler(RadioButtonCheckedChange);
-			((RadioButton)ControlDictionary["compiledAssemblyRadioButton"]).CheckedChanged += new EventHandler(RadioButtonCheckedChange);
-			((RadioButton)ControlDictionary["scriptFileRadioButton"]).CheckedChanged += new EventHandler(RadioButtonCheckedChange);
-			
-			(ControlDictionary["selectScriptFileButton"]).Click += new EventHandler(SelectScriptFileEvent);
-			(ControlDictionary["selectTargetButton"]).Click += new EventHandler(SelectTargetFolderEvent);
-			
-			this.project = (IProject)((IProperties)CustomizationObject).GetProperty("Project");
-			FileUtilityService fileUtilityService = (FileUtilityService)ServiceManager.Services.GetService(typeof(FileUtilityService));
-			foreach (ProjectFile info in project.ProjectFiles) {
-				if (info.BuildAction != BuildAction.Exclude) {
-					string name = fileUtilityService.AbsoluteToRelativePath(project.BaseDirectory, info.Name);
-					((CheckedListBox)ControlDictionary["projectFilesCheckedListBox"]).Items.Add(name, project.DeployInformation.IsFileExcluded(info.Name) ? CheckState.Unchecked : CheckState.Checked);
-			    }
-			}
-			
-			ControlDictionary["deployTargetTextBox"].Text = project.DeployInformation.DeployTarget;
-			ControlDictionary["deployScriptTextBox"].Text = project.DeployInformation.DeployScript;
-			
-			((RadioButton)ControlDictionary["projectFileRadioButton"]).Checked = project.DeployInformation.DeploymentStrategy == DeploymentStrategy.File;
-			((RadioButton)ControlDictionary["compiledAssemblyRadioButton"]).Checked = project.DeployInformation.DeploymentStrategy == DeploymentStrategy.Assembly;
-			((RadioButton)ControlDictionary["scriptFileRadioButton"]).Checked = project.DeployInformation.DeploymentStrategy == DeploymentStrategy.Script;
-			
-			RadioButtonCheckedChange(null, null);
+			Add (widget = new  DeployFileOptionsWidget ((IProperties) CustomizationObject));
 		}
 		
 		public override bool StorePanelContents()
 		{
-			if (ControlDictionary["deployTargetTextBox"].Text.Length > 0) {
-				if (!FileUtilityService.IsValidFileName(ControlDictionary["deployTargetTextBox"].Text)) {
-					MessageService.ShowError("Invalid deploy target specified");
-					return false;
-				}
-			}
-			
-			if (ControlDictionary["deployScriptTextBox"].Text.Length > 0) {
-				if (!FileUtilityService.IsValidFileName(ControlDictionary["deployScriptTextBox"].Text)) {
-					MessageService.ShowError("Invalid deploy script specified");
-					return false;
-				}
-				if (!File.Exists(ControlDictionary["deployScriptTextBox"].Text)) {
-					MessageService.ShowError("Deploy script doesn't exists");
-					return false;
-				}
-			}
-			
-			project.DeployInformation.DeployTarget = ControlDictionary["deployTargetTextBox"].Text;
-			project.DeployInformation.DeployScript = ControlDictionary["deployScriptTextBox"].Text;
-			
-			if (((RadioButton)ControlDictionary["projectFileRadioButton"]).Checked) {
-				project.DeployInformation.DeploymentStrategy = DeploymentStrategy.File;
-			} else if (((RadioButton)ControlDictionary["compiledAssemblyRadioButton"]).Checked) {
-				project.DeployInformation.DeploymentStrategy = DeploymentStrategy.Assembly;
-			} else {
-				project.DeployInformation.DeploymentStrategy = DeploymentStrategy.Script;
-			}
-			
-			project.DeployInformation.ClearExcludedFiles();
-			for (int i = 0; i < ((CheckedListBox)ControlDictionary["projectFilesCheckedListBox"]).Items.Count; ++i) {
-				if (!((CheckedListBox)ControlDictionary["projectFilesCheckedListBox"]).GetItemChecked(i)) {
-					project.DeployInformation.AddExcludedFile(fileUtilityService.RelativeToAbsolutePath(project.BaseDirectory, ((CheckedListBox)ControlDictionary["projectFilesCheckedListBox"]).Items[i].ToString()));
-				}
-			}
-			return true;
+			bool success = widget.Store();
+ 			return success;
 		}
-		
-		void SelectScriptFileEvent(object sender, EventArgs e)
-		{
-			using (OpenFileDialog fdiag  = new OpenFileDialog()) {
-				fdiag.CheckFileExists = true;
-				fdiag.Filter = StringParserService.Parse("${res:SharpDevelop.FileFilter.ExecutableFiles}|*.exe;*.com;*.pif;*.bat;*.cmd|${res:SharpDevelop.FileFilter.AllFiles}|*.*");
-				
-				if (fdiag.ShowDialog() == DialogResult.OK) {
-					ControlDictionary["deployScriptTextBox"].Text = fdiag.FileName;
-				}
-			}
-		}
-		
-		void SelectTargetFolderEvent(object sender, EventArgs e)
-		{
-			FolderDialog fdiag = new  FolderDialog();
-			if (fdiag.DisplayDialog("${res:Dialog.Options.PrjOptions.DeployFile.FolderDialogDescription}") == DialogResult.OK) {
-				ControlDictionary["deployTargetTextBox"].Text = fdiag.Path;
-			}
-		}
-		
-		void RadioButtonCheckedChange(object sender, EventArgs e)
-		{
-			ControlDictionary["deployTargetTextBox"].Enabled = ControlDictionary["selectTargetButton"].Enabled = ((RadioButton)ControlDictionary["projectFileRadioButton"]).Checked || ((RadioButton)ControlDictionary["compiledAssemblyRadioButton"]).Checked;
-			ControlDictionary["deployScriptTextBox"].Enabled = ControlDictionary["selectScriptFileButton"].Enabled = ((RadioButton)ControlDictionary["scriptFileRadioButton"]).Checked;
-		}
-	}*/
+	}
 }
