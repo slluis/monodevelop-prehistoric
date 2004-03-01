47905a1b7de5c75ae8280575f7f841ae59ec5cee
dkor <dkor@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2004-03-01 02:34:33 +0000

    Internationalized, Modified GeneralProjectPanelproperties in UI
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@1070 b5faff8f-5bce-0310-b493-a43f95cdeebf

--- MonoDevelop/src/Main/Base/Gui/Dialogs/OptionPanels/ProjectOptions/GeneralProjectOptions.cs
+++ MonoDevelop/src/Main/Base/Gui/Dialogs/OptionPanels/ProjectOptions/GeneralProjectOptions.cs
@@ -33,53 +33,82 @@ namespace ICSharpCode.SharpDevelop.Gui.Dialogs.OptionPanels
 		class GeneralProjectOptionsWidget : GladeWidgetExtract {
 
 			// Gtk Controls
-			[Glade.Widget] Entry ProjectNameEntry;
-			[Glade.Widget] TextView ProjectDescriptionTextView;
-			[Glade.Widget] CheckButton NewFilesOnLoadCheckButton;
- 			[Glade.Widget] CheckButton AutoInsertNewFilesCheckButton;
- 			[Glade.Widget] CheckButton EnableViewStateCheckButton;
+			[Glade.Widget] Label informationHeaderLabel;
+			[Glade.Widget] Label nameLabel;
+			[Glade.Widget] Label descriptionLabel;
+			[Glade.Widget] Label onProjectLoadHeaderLabel;
+			[Glade.Widget] Entry projectNameEntry;
+			[Glade.Widget] TextView projectDescriptionTextView;
+			[Glade.Widget] CheckButton newFilesOnLoadCheckButton;
+ 			[Glade.Widget] CheckButton autoInsertNewFilesCheckButton;
+ 			[Glade.Widget] CheckButton enableViewStateCheckButton;
 
+			// Services
+			StringParserService StringParserService = (StringParserService)ServiceManager.Services.GetService (
+							typeof (StringParserService));
 			IProject project;
+
+
 
 			public GeneralProjectOptionsWidget (IProperties CustomizationObject) : base ("Base.glade", "GeneralProjectOptionsPanel")
 			{
 				this.project = (IProject)((IProperties)CustomizationObject).GetProperty("Project");
+
+				nameLabel.Text = StringParserService.Parse(
+					"${res:Dialog.Options.PrjOptions.General.ProjectNameLabel}");
+				nameLabel.UseUnderline = true;
 				
-				ProjectNameEntry.Text = project.Name;
-				ProjectDescriptionTextView.Buffer.Text = project.Description;
-				EnableViewStateCheckButton.Active = project.EnableViewState;
+				descriptionLabel.Text = StringParserService.Parse(
+					"${res:Dialog.Options.PrjOptions.General.ProjectDescriptionLabel}");
+				descriptionLabel.UseUnderline = true;
+
+				// FIXME: il8n this
+				informationHeaderLabel.Markup = "<b>" + "Project Information" + "</b>";
+				// FIXME: il8n this
+				onProjectLoadHeaderLabel.Markup = "<b>" + "On Project Load" + "</b>";
+
+				newFilesOnLoadCheckButton.Label = StringParserService.Parse(
+					"${res:Dialog.Options.PrjOptions.General.SearchNewFileOnLoadCheckBox}");
+				autoInsertNewFilesCheckButton.Label = StringParserService.Parse(
+					"${res:Dialog.Options.PrjOptions.General.AutoIncludeFilesCheckBox}");
+				enableViewStateCheckButton.Label = StringParserService.Parse(
+					"${res:Dialog.Options.PrjOptions.General.ViewStateCheckBox}" );
+
+				projectNameEntry.Text = project.Name;
+				projectDescriptionTextView.Buffer.Text = project.Description;
+				enableViewStateCheckButton.Active = project.EnableViewState;
 				
 				switch (project.NewFileSearch) 
 				{
 				case NewFileSearch.None:
-					NewFilesOnLoadCheckButton.Active = false; 
-					AutoInsertNewFilesCheckButton.Active = false;
+					newFilesOnLoadCheckButton.Active = false; 
+					autoInsertNewFilesCheckButton.Active = false;
 					break;
 				case NewFileSearch.OnLoad:
-					NewFilesOnLoadCheckButton.Active = true; 
-					AutoInsertNewFilesCheckButton.Active = false;
+					newFilesOnLoadCheckButton.Active = true; 
+					autoInsertNewFilesCheckButton.Active = false;
 					break;
 				default:
-					NewFilesOnLoadCheckButton.Active = true; 
-					AutoInsertNewFilesCheckButton.Active = true;
+					newFilesOnLoadCheckButton.Active = true; 
+					autoInsertNewFilesCheckButton.Active = true;
 					break;
 				}
 				
-				NewFilesOnLoadCheckButton.Clicked += new EventHandler(AutoLoadCheckBoxCheckedChangeEvent);
+				newFilesOnLoadCheckButton.Clicked += new EventHandler(AutoLoadCheckBoxCheckedChangeEvent);
 				AutoLoadCheckBoxCheckedChangeEvent(null, null);
 			
 			}			
 		void AutoLoadCheckBoxCheckedChangeEvent(object sender, EventArgs e)
 		{
-			AutoInsertNewFilesCheckButton.Sensitive = false;
-			AutoInsertNewFilesCheckButton.Active = NewFilesOnLoadCheckButton.Active;
+			autoInsertNewFilesCheckButton.Sensitive = false;
+			autoInsertNewFilesCheckButton.Active = newFilesOnLoadCheckButton.Active;
 			
 			// FIXME: leave this deselected and checked by default for 0.1 release, uncomment again after release
  			/*
-			AutoInsertNewFilesCheckButton.Sensitive = NewFilesOnLoadCheckButton.Active;
-			if (NewFilesOnLoadCheckButton.Active == false) 
+			autoInsertNewFilesCheckButton.Sensitive = newFilesOnLoadCheckButton.Active;
+			if (newFilesOnLoadCheckButton.Active == false) 
 			{
-				AutoInsertNewFilesCheckButton.Active = false;
+				autoInsertNewFilesCheckButton.Active = false;
 			}
 			*/
 		}
@@ -87,12 +116,12 @@ namespace ICSharpCode.SharpDevelop.Gui.Dialogs.OptionPanels
 			
 			public void  Store (IProperties CustomizationObject)
 			{
-				project.Name                 = ProjectNameEntry.Text;
-				project.Description          = ProjectDescriptionTextView.Buffer.Text;
-				project.EnableViewState      = EnableViewStateCheckButton.Active;
+				project.Name                 = projectNameEntry.Text;
+				project.Description          = projectDescriptionTextView.Buffer.Text;
+				project.EnableViewState      = enableViewStateCheckButton.Active;
 				
-				if (NewFilesOnLoadCheckButton.Active) {
-					project.NewFileSearch = AutoInsertNewFilesCheckButton.Active ?  NewFileSearch.OnLoadAutoInsert : NewFileSearch.OnLoad;
+				if (newFilesOnLoadCheckButton.Active) {
+					project.NewFileSearch = autoInsertNewFilesCheckButton.Active ?  NewFileSearch.OnLoadAutoInsert : NewFileSearch.OnLoad;
 				} else {
 					project.NewFileSearch = NewFileSearch.None;
 				}
