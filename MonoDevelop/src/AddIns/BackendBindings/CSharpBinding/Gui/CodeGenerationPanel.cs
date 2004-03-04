5b95b0044859b48cad2fe0eb77155b8d59cbeacb
dkor <dkor@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2004-03-04 04:59:48 +0000

    Gladed/Ported CodeGenerationPanel under Project
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@1101 b5faff8f-5bce-0310-b493-a43f95cdeebf

--- MonoDevelop/src/AddIns/BackendBindings/CSharpBinding/Gui/CodeGenerationPanel.cs
+++ MonoDevelop/src/AddIns/BackendBindings/CSharpBinding/Gui/CodeGenerationPanel.cs
@@ -8,94 +8,116 @@
 using System;
 using System.IO;
 using System.Drawing;
-using System.Collections;
 
+using ICSharpCode.SharpDevelop.Internal.Project;
 using ICSharpCode.SharpDevelop.Internal.ExternalTool;
-using ICSharpCode.Core.AddIns.Codons;
-using ICSharpCode.Core.Properties;
-using ICSharpCode.SharpDevelop.Gui.Components;
+using ICSharpCode.SharpDevelop.Gui.Dialogs;
 using ICSharpCode.Core.Services;
-using ICSharpCode.Core.AddIns;
+using ICSharpCode.Core.Properties;
+using ICSharpCode.Core.AddIns.Codons;
 
 using Gtk;
+using MonoDevelop.Gui.Widgets;
 
-namespace ICSharpCode.SharpDevelop.Gui.Dialogs.OptionPanels
+namespace CSharpBinding
 {
+
+	//FIXME  i8n 
+
 	public class CodeGenerationPanel : AbstractOptionPanel
 	{
-		// Gtk Controls
-		CheckButton generateAdditonalCommentsCheckBox;
-		CheckButton generateDocCommentsCheckBox;
-		CheckButton useFullTypeNamesCheckBox; 
-		CheckButton blankLinesBetweenMemberCheckBox;
-		CheckButton elseOnClosingCheckBox;
-		CheckButton startBlockOnTheSameLineCheckBox; 
-		
-		// Services
-		StringParserService StringParserService = (StringParserService)ServiceManager.Services.GetService (typeof (StringParserService));
-		PropertyService p = (PropertyService)ServiceManager.Services.GetService (typeof (PropertyService));
-		static readonly string codeGenerationProperty = "SharpDevelop.UI.CodeGenerationOptions";
+
+		class CodeGenerationPanelWidget : GladeWidgetExtract 
+		{
+			//
+			// Gtk Controls	
+			//
+ 			[Glade.Widget] Entry symbolsEntry;
+ 			[Glade.Widget] Entry mainClassEntry;
+			[Glade.Widget] OptionMenu CompileTargetOptionMenu;
+ 			[Glade.Widget] CheckButton generateOverflowChecksCheckButton;
+			[Glade.Widget] CheckButton allowUnsafeCodeCheckButton;
+ 			[Glade.Widget] CheckButton enableOptimizationCheckButton;
+ 			[Glade.Widget] CheckButton warningsAsErrorsCheckButton;
+ 			[Glade.Widget] CheckButton generateDebugInformationCheckButton;
+ 			[Glade.Widget] CheckButton generateXmlOutputCheckButton;
+ 			[Glade.Widget] SpinButton warningLevelSpinButton;
+
+			//
+			// services needed
+			//
+			StringParserService StringParserService = (StringParserService)ServiceManager.Services.GetService (
+				typeof (StringParserService));
+
+			CSharpCompilerParameters compilerParameters = null;
+
+ 			public  CodeGenerationPanelWidget(IProperties CustomizationObject) : base ("CSharp.glade", "CodeGenerationPanel")
+ 			{	
+				this.compilerParameters = (CSharpCompilerParameters)((IProperties)CustomizationObject).GetProperty("Config");
+
+				Menu CompileTargetMenu = new Menu ();
+				CompileTargetMenu.Append(new MenuItem(
+								 StringParserService.Parse(
+									 "${res:Dialog.Options.PrjOptions.Configuration.CompileTarget.Exe}")));
+
+				CompileTargetMenu.Append(new MenuItem(
+								 StringParserService.Parse(
+									 "${res:Dialog.Options.PrjOptions.Configuration.CompileTarget.Library}")));
+				// FIXME commented until the Module capability is ported
+// 				CompileTargetMenu.Append(new MenuItem(
+// 								 StringParserService.Parse(
+// 									 "${res:Dialog.Options.PrjOptions.Configuration.CompileTarget.Module}")));
+
+				CompileTargetOptionMenu.Menu = CompileTargetMenu;
+				CompileTargetOptionMenu.SetHistory ( (uint) compilerParameters.CompileTarget);
+
+
+				symbolsEntry.Text = compilerParameters.DefineSymbols;
+				mainClassEntry.Text = compilerParameters.MainClass;
+
+				generateDebugInformationCheckButton.Active = compilerParameters.Debugmode;
+				generateXmlOutputCheckButton.Active        = compilerParameters.GenerateXmlDocumentation;
+				enableOptimizationCheckButton.Active       = compilerParameters.Optimize;
+				allowUnsafeCodeCheckButton.Active          = compilerParameters.UnsafeCode;
+				generateOverflowChecksCheckButton.Active   = compilerParameters.GenerateOverflowChecks;
+				warningsAsErrorsCheckButton.Active         = ! compilerParameters.RunWithWarnings;
+				warningLevelSpinButton.Value               = compilerParameters.WarningLevel;				
+ 			}
+
+			public bool Store ()
+			{	
+				if (compilerParameters == null) {
+					return true;
+				}
+				compilerParameters.CompileTarget =  (CompileTarget)  CompileTargetOptionMenu.History;
+				compilerParameters.DefineSymbols =  symbolsEntry.Text;
+				compilerParameters.MainClass     =  mainClassEntry.Text;
+
+				compilerParameters.Debugmode                = generateDebugInformationCheckButton.Active;
+				compilerParameters.GenerateXmlDocumentation = generateXmlOutputCheckButton.Active;
+				compilerParameters.Optimize                 = enableOptimizationCheckButton.Active;
+				compilerParameters.UnsafeCode               = allowUnsafeCodeCheckButton.Active;
+				compilerParameters.GenerateOverflowChecks   = generateOverflowChecksCheckButton.Active;
+				compilerParameters.RunWithWarnings          = ! warningsAsErrorsCheckButton.Active;
+
+				compilerParameters.WarningLevel = warningLevelSpinButton.ValueAsInt;
+
+				return true;
+			}
+		}
+
+		CodeGenerationPanelWidget widget;
 		
 		public override void LoadPanelContents()
 		{
-			// set up the form controls instance
-			SetupPanelInstance();
-			
-			
-			generateAdditonalCommentsCheckBox.Active = p.GetProperty("GenerateAdditionalComments", true);
-			generateDocCommentsCheckBox.Active       = p.GetProperty("GenerateDocumentComments", true);
-			useFullTypeNamesCheckBox.Active          = p.GetProperty("UseFullyQualifiedNames", true);
-			
-			blankLinesBetweenMemberCheckBox.Active   = p.GetProperty("BlankLinesBetweenMembers", true);
-			elseOnClosingCheckBox.Active             = p.GetProperty("ElseOnClosing", true);
-			startBlockOnTheSameLineCheckBox.Active   = p.GetProperty("StartBlockOnSameLine", true);
+			Add (widget = new  CodeGenerationPanelWidget ((IProperties) CustomizationObject));
 		}
 		
 		public override bool StorePanelContents()
 		{
-			p.SetProperty("GenerateAdditionalComments", generateAdditonalCommentsCheckBox.Active);
-			p.SetProperty("GenerateDocumentComments",   generateDocCommentsCheckBox.Active);
-			p.SetProperty("UseFullyQualifiedNames",     useFullTypeNamesCheckBox.Active);
-			p.SetProperty("BlankLinesBetweenMembers",   blankLinesBetweenMemberCheckBox.Active);
-			p.SetProperty("ElseOnClosing",              elseOnClosingCheckBox.Active);
-			p.SetProperty("StartBlockOnSameLine",       startBlockOnTheSameLineCheckBox.Active);
-			return true;
+			bool result = true;
+			result = widget.Store ();
+ 			return result;
 		}
-		
-		private void SetupPanelInstance()
-		{
-			// instantiate all the controls in the first group
-			Gtk.Frame frame1 = new Gtk.Frame(StringParserService.Parse("${res:Dialog.Options.IDEOptions.CodeGenerationOptionsPanel.CodeGenerationOptionsGroupBox}"));
-			Gtk.VBox vBox1 = new Gtk.VBox(false,2);			
-			useFullTypeNamesCheckBox = CheckButton.NewWithLabel(StringParserService.Parse("${res:Dialog.Options.IDEOptions.CodeGenerationOptionsPanel.UseFullTypeNamesCheckBox}")); 
-			blankLinesBetweenMemberCheckBox = CheckButton.NewWithLabel(StringParserService.Parse("${res:Dialog.Options.IDEOptions.CodeGenerationOptionsPanel.BlankLinesBetweenMembersCheckBox}"));
-			elseOnClosingCheckBox = CheckButton.NewWithLabel(StringParserService.Parse("${res:Dialog.Options.IDEOptions.CodeGenerationOptionsPanel.ElseOnClosingCheckBox}"));
-			startBlockOnTheSameLineCheckBox = CheckButton.NewWithLabel(StringParserService.Parse("${res:Dialog.Options.IDEOptions.CodeGenerationOptionsPanel.StartBlockOnTheSameLineCheckBox}")); 
-			
-			// pack all controls in first group
-			vBox1.PackStart(startBlockOnTheSameLineCheckBox, false, false, 2);
-			vBox1.PackStart(elseOnClosingCheckBox, false, false, 2);
-			vBox1.PackStart(blankLinesBetweenMemberCheckBox, false, false, 2);
-			vBox1.PackStart(useFullTypeNamesCheckBox, false, false, 2);
-			frame1.Add(vBox1);
-			
-			// instantiate all the controls in the second group
-			Gtk.Frame frame2 = new Gtk.Frame(StringParserService.Parse("${res:Dialog.Options.IDEOptions.CodeGenerationOptionsPanel.CommentGenerationOptionsGroupBox}"));
-			Gtk.VBox vBox2 = new Gtk.VBox(false,2);			
-			generateAdditonalCommentsCheckBox = CheckButton.NewWithLabel(StringParserService.Parse("${res:Dialog.Options.IDEOptions.CodeGenerationOptionsPanel.GenerateAdditionalCommentsCheckBox}"));
-			generateDocCommentsCheckBox = CheckButton.NewWithLabel(StringParserService.Parse("${res:Dialog.Options.IDEOptions.CodeGenerationOptionsPanel.GenerateDocCommentsCheckBox}"));
-			
-			// pack all controls in second group
-			vBox2.PackStart(generateDocCommentsCheckBox, false, false, 2);
-			vBox2.PackStart(generateAdditonalCommentsCheckBox, false, false, 2);
-			frame2.Add(vBox2);
-			
-			// pack all the groups
-			Gtk.VBox mainBox = new Gtk.VBox(false,2);
-			mainBox.PackStart(frame1, false, false, 2);
-			mainBox.PackStart(frame2, false, false, 2);
-			this.Add(mainBox);
-		}
-
 	}
 }
