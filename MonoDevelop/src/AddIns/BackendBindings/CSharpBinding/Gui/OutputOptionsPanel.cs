ae7b2df325f5a346cab9aec72a646b841c757ecd
dkor <dkor@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2004-03-04 07:06:15 +0000

    Gladed OutputOptionsPanel under project
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@1105 b5faff8f-5bce-0310-b493-a43f95cdeebf

--- MonoDevelop/src/AddIns/BackendBindings/CSharpBinding/Gui/OutputOptionsPanel.cs
+++ MonoDevelop/src/AddIns/BackendBindings/CSharpBinding/Gui/OutputOptionsPanel.cs
@@ -17,115 +17,145 @@ using ICSharpCode.Core.Properties;
 using ICSharpCode.Core.AddIns.Codons;
 
 using Gtk;
+using MonoDevelop.Gui.Widgets;
 
 namespace CSharpBinding
 {
+	
+	//FIXME: i8n 
+
 	public class OutputOptionsPanel : AbstractOptionPanel
 	{
-		CSharpCompilerParameters compilerParameters;
-		Entry assemblyNameTextBox = new Entry ();
-		Entry outputDirectoryTextBox = new Entry ();
-		Entry parametersTextBox = new Entry ();
-		Entry executeBeforeTextBox = new Entry ();
-		Entry executeScriptTextBox = new Entry ();
-		Entry executeAfterTextBox = new Entry ();
-		CheckButton pauseConsoleOutputCheckBox = new CheckButton ();
+
+		class OutputOptionsPanelWidget : GladeWidgetExtract 
+		{
+			//
+			// Gtk Controls	
+			//
+			[Glade.Widget] Entry assemblyNameEntry;
+			[Glade.Widget] Entry outputDirectoryEntry;
+			[Glade.Widget] Entry parametersEntry;
+			[Glade.Widget] Entry executeBeforeEntry;
+			[Glade.Widget] Entry executeScriptEntry;
+			[Glade.Widget] Entry executeAfterEntry;
+			[Glade.Widget] CheckButton pauseConsoleOutputCheckButton;			
+			[Glade.Widget] Button browseButton;
+			[Glade.Widget] Button browseButton2;
+			[Glade.Widget] Button browseButton3;
+			[Glade.Widget] Button browseButton4;
+			
+			CSharpCompilerParameters compilerParameters;
+
+			public  OutputOptionsPanelWidget(IProperties CustomizationObject) : base ("CSharp.glade", "OutputOptionsPanel")
+ 			{			
+				this.compilerParameters = (CSharpCompilerParameters)((IProperties)CustomizationObject).GetProperty("Config");
+				browseButton.Clicked += new EventHandler (SelectFolder);
+				browseButton2.Clicked += new EventHandler (SelectFile4);
+				browseButton3.Clicked += new EventHandler (SelectFile3);
+				browseButton4.Clicked += new EventHandler (SelectFile2);
+				
+				assemblyNameEntry.Text = compilerParameters.OutputAssembly;
+				outputDirectoryEntry.Text = compilerParameters.OutputDirectory;
+				parametersEntry.Text      = compilerParameters.CommandLineParameters;
+				executeScriptEntry.Text   = compilerParameters.ExecuteScript;
+ 				executeBeforeEntry.Text   = compilerParameters.ExecuteBeforeBuild;
+ 				executeAfterEntry.Text    = compilerParameters.ExecuteAfterBuild;
+				
+ 				pauseConsoleOutputCheckButton.Active = compilerParameters.PauseConsoleOutput;
+			}
+
+			public bool Store ()
+			{	
+				if (compilerParameters == null) {
+					return true;
+				}
+				
+				FileUtilityService fileUtilityService = (FileUtilityService)ServiceManager.Services.GetService(
+					typeof(FileUtilityService));
+				if (!fileUtilityService.IsValidFileName(assemblyNameEntry.Text)) {
+					//MessageService.ShowError("Invalid assembly name specified");
+					return false;
+				}
+				if (!fileUtilityService.IsValidFileName (outputDirectoryEntry.Text)) {
+					//MessageService.ShowError("Invalid output directory specified");
+					return false;
+				}
+				
+				compilerParameters.OutputAssembly = assemblyNameEntry.Text;
+				compilerParameters.OutputDirectory = outputDirectoryEntry.Text;
+				compilerParameters.CommandLineParameters = parametersEntry.Text;
+				compilerParameters.ExecuteBeforeBuild = executeBeforeEntry.Text;
+				compilerParameters.ExecuteAfterBuild = executeAfterEntry.Text;
+				compilerParameters.ExecuteScript = executeScriptEntry.Text;
+				
+				compilerParameters.PauseConsoleOutput = pauseConsoleOutputCheckButton.Active;
+				return true;
+			}
+			
+			void SelectFolder(object sender, EventArgs e)
+			{
+				FileSelection fdiag = new FileSelection (
+					"${res:Dialog.Options.PrjOptions.Configuration.FolderBrowserDescription}");
+				
+				if (fdiag.Run () == (int) ResponseType.Ok) {
+				outputDirectoryEntry.Text = fdiag.Filename;				
+				}
+				fdiag.Hide ();
+				fdiag.Dispose ();
+			}
 		
+			void SelectFile2(object sender, EventArgs e)
+			{
+				FileSelection fdiag = new FileSelection ("");
+				//fdiag.Filter = StringParserService.Parse("${res:SharpDevelop.FileFilter.AllFiles}|*.*");
+				fdiag.SelectMultiple = false;
+				
+				if(fdiag.Run () == (int) ResponseType.Ok) {
+					executeBeforeEntry.Text = fdiag.Filename;
+				}
+				fdiag.Hide ();
+				fdiag.Dispose ();			
+			}
+			
+			void SelectFile3(object sender, EventArgs e)
+			{
+				FileSelection fdiag = new FileSelection ("");
+				//fdiag.Filter = StringParserService.Parse("${res:SharpDevelop.FileFilter.AllFiles}|*.*");
+				fdiag.SelectMultiple = false;
+				
+				if(fdiag.Run () == (int) ResponseType.Ok) {
+					executeAfterEntry.Text = fdiag.Filename;
+				}
+				fdiag.Hide ();
+				fdiag.Dispose ();
+			}
+		
+			void SelectFile4(object sender, EventArgs e)
+			{
+				FileSelection fdiag = new FileSelection ("");
+				//fdiag.Filter = StringParserService.Parse("${res:SharpDevelop.FileFilter.AllFiles}|*.*");
+				fdiag.SelectMultiple = false;
+				
+				if(fdiag.Run () == (int) ResponseType.Ok) {
+					executeScriptEntry.Text = fdiag.Filename;
+				}
+				fdiag.Hide ();
+				fdiag.Dispose ();
+			}
+		}
+
+		OutputOptionsPanelWidget  widget;
+
 		public override void LoadPanelContents()
 		{
-			Button browseButton = new Button ();
-			Button browseButton2 = new Button ();
-			Button browseButton3 = new Button ();
-			Button browseButton4 = new Button ();
-			browseButton.Clicked += new EventHandler (SelectFolder);
-			browseButton2.Clicked += new EventHandler (SelectFile2);
-			browseButton3.Clicked += new EventHandler (SelectFile3);
-			browseButton4.Clicked += new EventHandler (SelectFile4);
-			
-			this.compilerParameters = (CSharpCompilerParameters)((IProperties)CustomizationObject).GetProperty("Config");
-			
-			//Console.WriteLine("SET BLABLUB");
-			assemblyNameTextBox.Text = compilerParameters.OutputAssembly;
-			outputDirectoryTextBox.Text = compilerParameters.OutputDirectory;
-			parametersTextBox.Text      = compilerParameters.CommandLineParameters;
-			executeScriptTextBox.Text   = compilerParameters.ExecuteScript;
-			executeBeforeTextBox.Text   = compilerParameters.ExecuteBeforeBuild;
-			executeAfterTextBox.Text    = compilerParameters.ExecuteAfterBuild;
-			
-			pauseConsoleOutputCheckBox.Active = compilerParameters.PauseConsoleOutput;
+			Add (widget = new  OutputOptionsPanelWidget ((IProperties) CustomizationObject));
 		}
 		
 		public override bool StorePanelContents()
 		{
-			//Console.WriteLine("store contents");
-			
-			if (compilerParameters == null) {
-				return true;
-			}
-			
-			//Console.WriteLine("1");
-			FileUtilityService fileUtilityService = (FileUtilityService)ServiceManager.Services.GetService(typeof(FileUtilityService));
-			if (!fileUtilityService.IsValidFileName(assemblyNameTextBox.Text)) {
-				//MessageService.ShowError("Invalid assembly name specified");
-				return false;
-			}
-			if (!fileUtilityService.IsValidFileName (outputDirectoryTextBox.Text)) {
-				//MessageService.ShowError("Invalid output directory specified");
-				return false;
-			}
-			
-			//Console.WriteLine("2");
-			compilerParameters.OutputAssembly = assemblyNameTextBox.Text;
-			compilerParameters.OutputDirectory = outputDirectoryTextBox.Text;
-			compilerParameters.CommandLineParameters = parametersTextBox.Text;
-			compilerParameters.ExecuteBeforeBuild = executeBeforeTextBox.Text;
-			compilerParameters.ExecuteAfterBuild = executeAfterTextBox.Text;
-			compilerParameters.ExecuteScript = executeScriptTextBox.Text;
-			
-			compilerParameters.PauseConsoleOutput = pauseConsoleOutputCheckBox.Active;
-			return true;
-		}
-		
-		void SelectFolder(object sender, EventArgs e)
-		{
-			FileSelection fdiag = new FileSelection ("${res:Dialog.Options.PrjOptions.Con        figuration.FolderBrowserDescription}");
-			
-			if (fdiag.Run () == (int) ResponseType.Ok) {
-				outputDirectoryTextBox.Text = fdiag.Filename;				
-			}
-		}
-		
-		void SelectFile2(object sender, EventArgs e)
-		{
-			FileSelection fdiag = new FileSelection ("");
-			//fdiag.Filter = StringParserService.Parse("${res:SharpDevelop.FileFilter.AllFiles}|*.*");
-			fdiag.SelectMultiple = false;
-			
-			if(fdiag.Run () == (int) ResponseType.Ok) {
-				executeBeforeTextBox.Text = fdiag.Filename;
-			}
-		}
-		
-		void SelectFile3(object sender, EventArgs e)
-		{
-			FileSelection fdiag = new FileSelection ("");
-			//fdiag.Filter = StringParserService.Parse("${res:SharpDevelop.FileFilter.AllFiles}|*.*");
-			fdiag.SelectMultiple = false;
-			
-			if(fdiag.Run () == (int) ResponseType.Ok) {
-				executeAfterTextBox.Text = fdiag.Filename;
-			}
-		}
-		
-		void SelectFile4(object sender, EventArgs e)
-		{
-			FileSelection fdiag = new FileSelection ("");
-			//fdiag.Filter = StringParserService.Parse("${res:SharpDevelop.FileFilter.AllFiles}|*.*");
-			fdiag.SelectMultiple = false;
-			
-			if(fdiag.Run () == (int) ResponseType.Ok) {
-				executeScriptTextBox.Text = fdiag.Filename;
-			}
+			bool result = true;
+			result = widget.Store ();
+ 			return result;
 		}
 	}
 }
