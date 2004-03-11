205afe3fcfc5b1aeb90cbf7e2ebf13181e708971
dkor <dkor@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2004-03-11 19:56:58 +0000

    Finishing Bug 53845: Changed ProjectAndCombinePanel names to BuildPanel names
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@1143 b5faff8f-5bce-0310-b493-a43f95cdeebf

---
+++ MonoDevelop/src/Main/Base/Gui/Dialogs/OptionPanels/IDEOptions/BuildPanel.cs
@@ -0,0 +1,117 @@
+// <file>
+//     <copyright see="prj:///doc/copyright.txt"/>
+//     <license see="prj:///doc/license.txt"/>
+//     <owner name="Mike Krüger" email="mike@icsharpcode.net"/>
+//     <version value="$version"/>
+// </file>
+
+using System;
+using System.IO;
+using System.Drawing;
+using System.Collections;
+
+using MonoDevelop.Internal.ExternalTool;
+using MonoDevelop.Core.AddIns.Codons;
+using MonoDevelop.Core.Properties;
+using MonoDevelop.Gui.Components;
+using MonoDevelop.Core.Services;
+using MonoDevelop.Core.AddIns;
+using MonoDevelop.Services;
+
+//using System.Windows.Forms;
+using Gtk;
+using MonoDevelop.Gui.Widgets;
+
+namespace MonoDevelop.Gui.Dialogs.OptionPanels
+{
+	public class BuildPanel : AbstractOptionPanel
+	{
+
+		// service instances needed
+		PropertyService PropertyService = (PropertyService)ServiceManager.Services.GetService (typeof (PropertyService));
+		
+		BuildPanelWidget widget;
+		const string projectAndCombineProperty = "SharpDevelop.UI.ProjectAndCombineOptions";
+
+		public override void LoadPanelContents()
+		{
+			Add (widget = new  BuildPanelWidget ());
+		}
+		
+		public override bool StorePanelContents()
+		{
+			widget.Store ();
+			return true;
+		}
+		
+		public class BuildPanelWidget :  GladeWidgetExtract 
+		{
+			//
+			// Gtk controls
+			//
+
+			[Glade.Widget] public Gtk.RadioButton saveChangesRadioButton;
+			[Glade.Widget] public Gtk.RadioButton promptChangesRadioButton; 
+			[Glade.Widget] public Gtk.RadioButton noSaveRadioButton;
+			[Glade.Widget] public Gtk.CheckButton showTaskListCheckBox;
+			[Glade.Widget] public Gtk.CheckButton showOutputCheckBox;
+			[Glade.Widget] public Gtk.Label buildAndRunOptionsLabel;   
+
+			// service instances needed
+			StringParserService StringParserService = (StringParserService)ServiceManager.Services.GetService (
+				typeof (StringParserService));
+			PropertyService PropertyService = (PropertyService)ServiceManager.Services.GetService (
+				typeof (PropertyService));
+
+			public  BuildPanelWidget () : base ("Base.glade", "BuildPanel")
+			{
+			        //
+			        // getting internationalized strings
+			        //
+				// FIXME i8n the following line
+				buildAndRunOptionsLabel.Text = "Build and run options";
+				saveChangesRadioButton.Label = StringParserService.Parse(
+					"${res:Dialog.Options.IDEOptions.ProjectAndCombineOptions.SaveChangesRadioButton}");
+				promptChangesRadioButton.Label = StringParserService.Parse(
+					"${res:Dialog.Options.IDEOptions.ProjectAndCombineOptions.PromptToSaveRadioButton}");
+				noSaveRadioButton.Label = StringParserService.Parse(
+					"${res:Dialog.Options.IDEOptions.ProjectAndCombineOptions.DontSaveRadioButton}");
+				showOutputCheckBox.Label = StringParserService.Parse(
+					"${res:Dialog.Options.IDEOptions.ProjectAndCombineOptions.ShowOutputPadCheckBox}");
+				showTaskListCheckBox.Label = StringParserService.Parse(
+					"${res:Dialog.Options.IDEOptions.ProjectAndCombineOptions.ShowTaskListPadCheckBox}");
+				//
+				// reading properties
+				//
+				BeforeCompileAction action = (BeforeCompileAction) PropertyService.GetProperty(
+					"SharpDevelop.Services.DefaultParserService.BeforeCompileAction", 
+					BeforeCompileAction.SaveAllFiles);
+				saveChangesRadioButton.Active = action.Equals(BeforeCompileAction.SaveAllFiles);
+				promptChangesRadioButton.Active = action.Equals(BeforeCompileAction.PromptForSave);
+				noSaveRadioButton.Active = action.Equals(BeforeCompileAction.Nothing);
+				showTaskListCheckBox.Active = (bool)PropertyService.GetProperty(
+					"SharpDevelop.ShowTaskListAfterBuild", true);
+				showOutputCheckBox.Active = (bool)PropertyService.GetProperty(
+					"SharpDevelop.ShowOutputWindowAtBuild", true);
+			}
+			
+			public void Store ()
+			{
+				// set properties
+				if (saveChangesRadioButton.Active) {
+					PropertyService.SetProperty("SharpDevelop.Services.DefaultParserService.BeforeCompileAction", 
+							BeforeCompileAction.SaveAllFiles);
+				} else if (promptChangesRadioButton.Active) {
+					PropertyService.SetProperty("SharpDevelop.Services.DefaultParserService.BeforeCompileAction", 
+							BeforeCompileAction.PromptForSave);
+				} else if (noSaveRadioButton.Active) {
+					PropertyService.SetProperty("SharpDevelop.Services.DefaultParserService.BeforeCompileAction", 
+							BeforeCompileAction.Nothing);
+				}
+				
+				PropertyService.SetProperty("SharpDevelop.ShowTaskListAfterBuild", showTaskListCheckBox.Active);
+				PropertyService.SetProperty("SharpDevelop.ShowOutputWindowAtBuild", showOutputCheckBox.Active);
+			}
+		}
+	}
+}
