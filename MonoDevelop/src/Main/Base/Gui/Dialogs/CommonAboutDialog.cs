ff35d2b5000d5bb10b7b976091fa65e3292b3cd7
tberman <tberman@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2004-04-02 10:14:24 +0000

    gonna have to get used to updating po/POTFILES.in
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@1343 b5faff8f-5bce-0310-b493-a43f95cdeebf

--- MonoDevelop/src/Main/Base/Gui/Dialogs/CommonAboutDialog.cs
+++ MonoDevelop/src/Main/Base/Gui/Dialogs/CommonAboutDialog.cs
@@ -145,6 +145,7 @@ namespace MonoDevelop.Gui.Dialogs
 			nb.AppendPage (new AboutMonoDevelopTabPage (), new Label (GettextCatalog.GetString ("About MonoDevelop")));
 			//nb.AppendPage (aatp, new Label ("Authors"));
 			//nb.AppendPage (changelog, new Label ("ChangeLog"));
+
 			nb.AppendPage (vinfo, new Label (GettextCatalog.GetString ("Version Info")));
 			this.VBox.PackStart (nb);
 			this.AddButton (Gtk.Stock.Close, (int) ResponseType.Close);
