b660a57975d8e77a6f856da2277d6926c5baa8f3
deboald <deboald@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2004-04-05 11:11:23 +0000

    svn tells me this is modified - I think I changed it weeks ago :)
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@1386 b5faff8f-5bce-0310-b493-a43f95cdeebf

--- md-website/startserver.sh
+++ md-website/startserver.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# clean wapi, if necessary
+rm -rf $HOME/.wapi
+
 # start the mod-mono-server assembly
 
 mono /usr/local/bin/mod-mono-server.exe --root . --applications /:. --nonstop
 chmod 666 /tmp/mod_mono_server
+
+/etc/init.d/apache2 restart
