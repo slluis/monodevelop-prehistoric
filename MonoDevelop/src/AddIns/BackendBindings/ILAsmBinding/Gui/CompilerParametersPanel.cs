1f40e9d36be5295077036207004af1e37997b59b
jluke <jluke@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2004-04-26 05:18:31 +0000

    some ugly but functional ilasm project options
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@1512 b5faff8f-5bce-0310-b493-a43f95cdeebf

--- MonoDevelop/src/AddIns/BackendBindings/ILAsmBinding/Gui/CompilerParametersPanel.cs
+++ MonoDevelop/src/AddIns/BackendBindings/ILAsmBinding/Gui/CompilerParametersPanel.cs
@@ -15,26 +15,71 @@ using MonoDevelop.Gui.Dialogs;
 using MonoDevelop.Core.Services;
 using MonoDevelop.Core.Properties;
 using MonoDevelop.Core.AddIns.Codons;
+using MonoDevelop.Services;
 
 namespace ILAsmBinding
 {
 	public class CompilerParametersPanel : AbstractOptionPanel
 	{
 		ILAsmCompilerParameters compilerParameters = null;
+		Entry outputPath = new Entry ();
+		Entry assemblyName = new Entry ();
+		RadioButton exeTarget = new RadioButton ("exe");
+		RadioButton dllTarget;
+		CheckButton debug = new CheckButton (GettextCatalog.GetString ("Include debug information"));
 		
 		public override void LoadPanelContents()
 		{
 			this.compilerParameters = (ILAsmCompilerParameters)((IProperties)CustomizationObject).GetProperty("Config");
 			
-			//System.Windows.Forms.PropertyGrid grid = new System.Windows.Forms.PropertyGrid();
-			//grid.Dock = DockStyle.Fill;
-			//grid.SelectedObjects = new object[] { compilerParameters};
-			//Controls.Add(grid);
+			dllTarget = new RadioButton (exeTarget, "dll");
+			SetupUI ();
+			RestoreValues ();
+			this.ShowAll ();
 		}
 		
 		public override bool StorePanelContents()
 		{
+			compilerParameters.AssemblyName = assemblyName.Text;
+			compilerParameters.OutputPath = outputPath.Text;
+			compilerParameters.IncludeDebugInformation = debug.Active;
+			if (exeTarget.Active)
+				compilerParameters.CompilationTarget = CompilationTarget.Exe;
+			else
+				compilerParameters.CompilationTarget = CompilationTarget.Dll;
+				
 			return true;
+		}
+
+		void SetupUI ()
+		{
+			VBox vbox = new VBox (false, 6);
+			Label outputLabel = new Label ();
+			outputLabel.Markup = String.Format ("<b>{0}</b>", GettextCatalog.GetString ("Output path"));
+			vbox.PackStart (outputLabel, false, true, 0);
+			vbox.PackStart (outputPath, false, true, 0);
+			Label assemblyLabel = new Label ();
+			assemblyLabel.Markup = String.Format ("<b>{0}</b>", GettextCatalog.GetString ("Assembly name"));
+			vbox.PackStart (assemblyLabel, false, true, 0);
+			vbox.PackStart (assemblyName, false, true, 0);
+			Label targetLabel = new Label ();
+			targetLabel.Markup = String.Format ("<b>{0}</b>", GettextCatalog.GetString ("Target options"));
+			vbox.PackStart (targetLabel, false, true, 0);
+			vbox.PackStart (exeTarget, false, true, 0);
+			vbox.PackStart (dllTarget, false, true, 0);
+			vbox.PackStart (debug, false, true, 0);
+			this.Add (vbox);
+		}
+
+		void RestoreValues ()
+		{
+			assemblyName.Text = compilerParameters.AssemblyName;
+			outputPath.Text = compilerParameters.OutputPath;
+			if (compilerParameters.CompilationTarget == CompilationTarget.Exe)
+				exeTarget.Active = true;
+			else
+				dllTarget.Active = true;
+			debug.Active = compilerParameters.IncludeDebugInformation;
 		}
 	}
 }
