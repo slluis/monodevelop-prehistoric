4810ac80fb31365feda9285b7ceadfce41a6d407
deboald <deboald@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2004-05-04 23:29:47 +0000

    Just a note for myself
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@1576 b5faff8f-5bce-0310-b493-a43f95cdeebf

--- md-website/startserver.sh
+++ md-website/startserver.sh
@@ -2,10 +2,13 @@
 
 # clean wapi, if necessary
 rm -rf $HOME/.wapi
+rm /tmp/mod_mono_server
+
+# set PATH
+export PATH=$PATH:/usr/bin
 
 # start the mod-mono-server assembly
-
-mono /usr/local/bin/mod-mono-server.exe --root . --applications /:. --nonstop
+mono /usr/bin/mod-mono-server.exe --root . --applications /:. --nonstop
 chmod 666 /tmp/mod_mono_server
 
-/etc/init.d/apache2 restart
+/etc/init.d/httpd restart
