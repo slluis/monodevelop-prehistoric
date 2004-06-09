c340376d13622ca713dff3939c0d1c44b8142b49
tberman <tberman@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2004-06-09 01:20:53 +0000

    small test
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@1721 b5faff8f-5bce-0310-b493-a43f95cdeebf

---
+++ MonoDevelop/src/Libraries/Gdl/GdlDockTest.cs
@@ -0,0 +1,58 @@
+using System;
+using Gtk;
+using GtkSharp;
+using Gnome;
+using GnomeSharp;
+using Gdl;
+
+class T
+{
+	Program program;
+	
+	static void Main (string[] args)
+	{
+		new T (args);
+	}
+	
+	T (string[] args)
+	{
+		//program = new Program ("test", "0.0", Modules.UI, args);
+		//App app = new App ("test", "Test for Gdl.Dock widget");
+		Gtk.Application.Init ();
+		Gtk.Window app = new Gtk.Window ("test");
+		app.SetDefaultSize (600, 450);
+		app.DeleteEvent += new DeleteEventHandler (OnAppDelete);
+		
+		Dock dock = new Dock ();
+		//DockLayout layout = new DockLayout (dock);
+		
+		DockItem di = new DockItem ("item1", "Item #1", Gtk.Stock.Execute, DockItemBehavior.Normal);
+		di.Add (new Label ("test"));
+		dock.AddItem (di, DockPlacement.Center);
+		
+		/*DockItem di2 = new DockItem ("item2", "Item #2", DockItemBehavior.Normal);
+		di2.Add (new Label ("test2"));
+		dock.AddItem (di2, DockPlacement.Center);
+		
+		DockItem di3 = new DockItem ("item3", "Item #3", DockItemBehavior.Normal);
+		di3.Add (new Label ("test3"));
+		dock.AddItem (di3, DockPlacement.Center);
+		
+		DockItem di4 = new DockItem ("item4", "Item #4", DockItemBehavior.Normal);
+		di4.Add (new Label ("test4"));
+		dock.AddItem (di4, DockPlacement.Center);*/
+		
+		Console.WriteLine ("1");
+		app.Add (dock);
+		Console.WriteLine ("2");
+		app.ShowAll ();
+		Console.WriteLine ("3");
+		Gtk.Application.Run ();
+		Console.WriteLine ("4");
+	}
+	
+	private void OnAppDelete (object o, DeleteEventArgs args)
+	{
+		Gtk.Application.Quit ();
+	}
+}
