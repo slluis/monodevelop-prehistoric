694085a5c1821432cca2b457c7022875270a9cb0
jluke <jluke@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2004-06-11 05:08:32 +0000

    make toggle work
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@1737 b5faff8f-5bce-0310-b493-a43f95cdeebf

--- MonoDevelop/src/AddIns/Misc/AddInManager/Gui/AddInManagerDialog.cs
+++ MonoDevelop/src/AddIns/Misc/AddInManager/Gui/AddInManagerDialog.cs
@@ -9,6 +9,8 @@ namespace AddInManager
 {
 	public class AddInManagerDialog : Dialog
 	{
+		TreeStore store;
+
 		public AddInManagerDialog ()
 		{
 			this.BorderWidth = 12;
@@ -18,29 +20,44 @@ namespace AddInManager
 
 			ScrolledWindow sw = new ScrolledWindow ();
 			TreeView tv = new TreeView ();
-			tv.AppendColumn (GettextCatalog.GetString ("Enabled"), new CellRendererToggle (), "active", 0);
+
+			CellRendererToggle toggle = new CellRendererToggle ();
+			toggle.Toggled += OnCellToggled;
+			tv.AppendColumn (GettextCatalog.GetString ("Enabled"), toggle, "active", 0);
 			tv.AppendColumn (GettextCatalog.GetString ("Title"), new CellRendererText (), "text", 1);
 			tv.AppendColumn (GettextCatalog.GetString ("Version"), new CellRendererText (), "text", 2);
 			sw.Add (tv);
 
 			this.AddButton (Gtk.Stock.Close, ResponseType.Close);
 	
-			tv.Model = LoadAddIns ();
+			LoadAddIns ();
+			tv.Model = store;
 			this.VBox.Add (sw);
 			this.ShowAll ();
 		}
 
-		TreeStore LoadAddIns ()
+		void LoadAddIns ()
 		{
-			TreeStore store = new TreeStore (typeof (bool), typeof (string), typeof (string));
+			store = new TreeStore (typeof (bool), typeof (string), typeof (string));
 			AddInCollection addins = AddInTreeSingleton.AddInTree.AddIns;
 
 			foreach (AddIn a in addins)
 			{
 				store.AppendValues (true, a.Name, a.Version);
 			}
-		
-			return store;
+		}
+
+		void OnCellToggled (object o, ToggledArgs args)
+		{
+			CellRendererToggle toggle = (CellRendererToggle) o;
+                                                                                
+                        TreeIter iter;
+                        if (store.GetIterFromString(out iter, args.Path))
+                        {
+                                bool val = (bool) store.GetValue(iter, 0);
+                                store.SetValue (iter, 0, !val);
+                        }
+
 		}
 	}
 }
