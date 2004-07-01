fa489ceaa91657d2ebd1f78d3bca691179d84460
jluke <jluke@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2004-07-01 01:24:58 +0000

    add hack to check for Gtk 2.4
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@1867 b5faff8f-5bce-0310-b493-a43f95cdeebf

--- MonoDevelop/Core/src/Libraries/MonoDevelop.Gui.Utils/Glue.cs
+++ MonoDevelop/Core/src/Libraries/MonoDevelop.Gui.Utils/Glue.cs
@@ -18,6 +18,20 @@ namespace MonoDevelop.Gui.Utils
 		{
 			lmd_propagate_eventkey (Handle, ref key);
 		}
+
+		[DllImport ("gtk-x11-2.0")]
+		static extern IntPtr gtk_check_version (uint maj, uint min, uint mic);
+
+		// check for gtk 2.4 or newer
+		public bool IsGtk24 {
+			get {
+				string res = Marshal.PtrToStringAnsi (gtk_check_version (2, 4, 0));
+				if (res == null || res == String.Empty)
+					return true;
+				else
+					return false;
+			}
+		}
 	
 	}
 
