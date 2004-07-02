2173188904f90e189a03168240c9ca3617aff798
jluke <jluke@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2004-07-02 00:44:51 +0000

    2004-07-01  John Luke  <jluke@cfl.rr.com>
            * Commands/AutoStartCommands.cs: remove old Code generation,
            assembly preload, and tip of the day on start, because they
            are no longer used, and I believe no longer will be
            someone correct me if I am wrong
    
    
    
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@1879 b5faff8f-5bce-0310-b493-a43f95cdeebf

--- MonoDevelop/Core/src/Main/Base/Commands/AutostartCommands.cs
+++ MonoDevelop/Core/src/Main/Base/Commands/AutostartCommands.cs
@@ -6,32 +6,23 @@
 // </file>
 
 using System;
-using System.IO;
-using System.Collections;
-using System.CodeDom.Compiler;
-using System.Reflection;
-using System.Threading;
-using System.Runtime.Remoting;
-using System.Security.Policy;
 
 using MonoDevelop.Core.Services;
 using MonoDevelop.Core.Properties;
 using MonoDevelop.Core.AddIns.Codons;
 
-using MonoDevelop.Services;
 using MonoDevelop.Gui;
 using MonoDevelop.Gui.Dialogs;
 using MonoDevelop.Gui.ErrorHandlers;
 
-using SA = MonoDevelop.SharpAssembly.Assembly;
-
 using MonoDevelop.Internal.Parser;
+using MonoDevelop.Services;
+using SA = MonoDevelop.SharpAssembly.Assembly;
 
 namespace MonoDevelop.Commands
 {
 	public class InitializeWorkbenchCommand : AbstractCommand
 	{
-
 		public override void Run()
 		{
 			DefaultWorkbench w = new DefaultWorkbench();
@@ -43,34 +34,6 @@ namespace MonoDevelop.Commands
 		}
 	}
 	
-	public class StartCodeCompletionWizard : AbstractCommand
-	{
-
-		public static bool generatingCompletionData = false;
-
-		public override void Run()
-		{
-			PropertyService propertyService = (PropertyService)ServiceManager.GetService(typeof(PropertyService));
-			string path = propertyService.GetProperty ("SharpDevelop.CodeCompletion.DataDirectory", null);
-			string codeCompletionProxyFile = Path.Combine (path, "mscorlib.pidb");
-			//Console.WriteLine("checking for existence of {0}", codeCompletionProxyFile);
-
-/*			if (!File.Exists (codeCompletionProxyFile)) {
-				generatingCompletionData = true;
-				RunWizard();
-			}
-*/		}
-		
-		/*void RunWizard()
-		{
-			if (SplashScreenForm.SplashScreen.Visible) {
-				SplashScreenForm.SplashScreen.Hide();
-			}
-			
-			(new GenerateDatabase()).Start();
-		}*/
-	}
-	
 	public class StartParserServiceThread : AbstractCommand
 	{
 		public override void Run()
@@ -79,78 +42,18 @@ namespace MonoDevelop.Commands
 			parserService.StartParserThread();
 		}
 	}
-	
-	public class StartSharpAssemblyPreloadThread : AbstractCommand
-	{
-		public override void Run()
-		{
-			Thread preloadThread = new Thread(new ThreadStart(PreloadThreadStart));
-			preloadThread.IsBackground = true;
-			preloadThread.Priority = ThreadPriority.Lowest;
-			preloadThread.Start();
-		}
-		
-		public void PreloadThreadStart()
-		{
-			Console.WriteLine("#Assembly: starting preloading thread");
-			SA.SharpAssembly.Load("System");
-			Console.WriteLine("#Assembly: preloaded system");
-			SA.SharpAssembly.Load("System.Xml");
-			Console.WriteLine("#Assembly: preloaded system.xml");
-			SA.SharpAssembly.Load("System.Windows.Forms");
-			Console.WriteLine("#Assembly: preloaded system.windows.forms");
-			SA.SharpAssembly.Load("System.Drawing");
-			Console.WriteLine("#Assembly: preloaded system.drawing");
-			SA.SharpAssembly.Load("System.Data");
-			Console.WriteLine("#Assembly: preloaded system.data");
-			SA.SharpAssembly.Load("System.Design");			
-			Console.WriteLine("#Assembly: preloaded system.design");
-			SA.SharpAssembly.Load("System.Web");			
-			Console.WriteLine("#Assembly: preloaded system.web");
-		}
-	}
-	
+
 	public class StartWorkbenchCommand : AbstractCommand
 	{
 		const string workbenchMemento = "SharpDevelop.Workbench.WorkbenchMemento";
 		
-		EventHandler idleEventHandler;
-		bool isCalled = false;
-		
-		/// <remarks>
-		/// The worst workaround in the whole project
-		/// </remarks>
-		void ShowTipOfTheDay(object sender, EventArgs e)
-		{
-			if (isCalled) {
-				//Application.Idle -= idleEventHandler;
-				return;
-			}
-			isCalled = true;
-			// show tip of the day
-			PropertyService propertyService = (PropertyService)ServiceManager.GetService(typeof(PropertyService));
-			if (propertyService.GetProperty("MonoDevelop.Gui.Dialog.TipOfTheDayView.ShowTipsAtStartup", false)) {
-				ViewTipOfTheDay dview = new ViewTipOfTheDay();
-				dview.Run();
-			}
-		}
-		
 		public override void Run()
 		{
-
-			if (StartCodeCompletionWizard.generatingCompletionData) {
-				Gtk.Application.Run ();
-				return;
-			}
-		
 			// register string tag provider (TODO: move to add-in tree :)
 			StringParserService stringParserService = (StringParserService)ServiceManager.GetService(typeof(StringParserService));
 			stringParserService.RegisterStringTagProvider(new MonoDevelop.Commands.SharpDevelopStringTagProvider());
 			
 			PropertyService propertyService = (PropertyService)ServiceManager.GetService(typeof(PropertyService));
-			
-			//idleEventHandler = new EventHandler(ShowTipOfTheDay);
-			//Application.Idle += idleEventHandler;
 			
 			IProjectService projectService = (IProjectService)MonoDevelop.Core.Services.ServiceManager.GetService(typeof(IProjectService));
 			
@@ -194,12 +97,6 @@ namespace MonoDevelop.Commands
 			((Gtk.Window)WorkbenchSingleton.Workbench).Visible = true;
 			WorkbenchSingleton.Workbench.RedrawAllComponents ();
 			((Gtk.Window)WorkbenchSingleton.Workbench).Present ();
-		
-			// Give Gtk time to display the workbench window before showing the TOTD.
-			//while (Gtk.Application.EventsPending ())
-			//	Gtk.Application.RunIteration ();
-		
-			//ShowTipOfTheDay (null, null);
 		
 			// finally run the workbench window ...
 			Gtk.Application.Run ();
