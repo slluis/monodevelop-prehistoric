c49f734de66dd7e863ea400d30b891f1cf6c3f79
jluke <jluke@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2004-12-01 05:51:55 +0000

    2004-12-01  John Luke  <john.luke@gmail.com>
            * FileSelector/FileSelector.cs: improve this to take advantage
            of the FileChooser and remove my old hacks that were likely buggy
            use a switch for adding the buttons
            add ~/Projects as a MD specific bookmark
    
    
    
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@2041 b5faff8f-5bce-0310-b493-a43f95cdeebf

--- MonoDevelop/Core/src/Libraries/MonoDevelop.Gui.Widgets/FileSelector/FileSelector.cs
+++ MonoDevelop/Core/src/Libraries/MonoDevelop.Gui.Widgets/FileSelector/FileSelector.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using Gtk;
 
 using MonoDevelop.Services;
@@ -7,13 +6,9 @@ using MonoDevelop.Core.Services;
 
 namespace MonoDevelop.Gui.Widgets
 {
-	// basically just to remember the last directory
-	// we could do some if GTK2.4 then use new FileChooser
-	// but that is probably to be hacky at best
 	public class FileSelector : FileChooserDialog
 	{
 		const string LastPathProperty = "MonoDevelop.FileSelector.LastPath";
-		string lastPath;
 		PropertyService propertyService = (PropertyService) ServiceManager.GetService (typeof (PropertyService));
 
 		public FileSelector () : base (GettextCatalog.GetString ("Open file ..."), null, FileChooserAction.Open)
@@ -32,60 +27,42 @@ namespace MonoDevelop.Gui.Widgets
 
 		public FileSelector (string title, FileChooserAction action) : base (title, null, action)
 		{
-			if (action == FileChooserAction.SelectFolder) {
-				AddButton (Gtk.Stock.Cancel, ResponseType.Cancel);
-				AddButton ("Select Folder", ResponseType.Ok);
-			} else if (action == FileChooserAction.Save) {
-				AddButton (Gtk.Stock.Cancel, ResponseType.Cancel);
-				AddButton (Gtk.Stock.Save, ResponseType.Ok);
+			switch (action) {
+				case FileChooserAction.SelectFolder:
+					AddButton (Gtk.Stock.Cancel, ResponseType.Cancel);
+					AddButton ("Select Folder", ResponseType.Ok);
+					break;
+				case FileChooserAction.Save:
+					AddButton (Gtk.Stock.Cancel, ResponseType.Cancel);
+					AddButton (Gtk.Stock.Save, ResponseType.Ok);
+					break;
+				default:
+					break;
 			}
+
 			CommonSetup ();
 		}
 
 		void CommonSetup ()
 		{
 			// Restore the last active directory
-			string tmp = (string) propertyService.GetProperty (LastPathProperty);
-			if (tmp != null && tmp.Length > 0)
-			{
-				if (tmp.EndsWith ("/"))
-					lastPath = String.Format ("{0}", tmp.Trim ());
-				else
-					lastPath = String.Format ("{0}/", tmp.Trim ());
-			}
+			string last = (string) propertyService.GetProperty (LastPathProperty);
+			if (last != null && last.Length > 0)
+				this.SetCurrentFolder (last);
 			else
-			{
-				// FIXME: use ~/DefaultPath?
-				lastPath = Environment.GetEnvironmentVariable ("HOME");
-			}
+				this.SetCurrentFolder (Environment.GetFolderPath (Environment.SpecialFolder.Personal));
 
-			// Set the dir here, must end in "/" to work right
-			this.SetFilename (lastPath);
+			// add ~/Projects as a MD bookmark
+			this.AddShortcutFolder (System.IO.Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.Personal), "Projects"));
 
-			// Basically need to track if the directory has
-			// been changed in the simplest way possible
-			// I think that this always changes when the dir does
-			this.CurrentFolderChanged += OnOptionListChanged;
+			// FIXME: only set this once per-dialog
+			// perhaps in Dispose ()? or only when a file or dir is selected
+			this.CurrentFolderChanged += OnCurrentFolderChanged;
 		}
 
-		void OnOptionListChanged (object o, EventArgs args)
+		void OnCurrentFolderChanged (object o, EventArgs args)
 		{
-			UpdateLastDir ();
-		}
-
-		void UpdateLastDir ()
-		{
-			if (this.Filename == null || this.Filename == "")
-				return;
-			
-			if (this.Filename.EndsWith ("/") || Directory.Exists (this.Filename))
-				lastPath = this.Filename;
-			else
-				lastPath = System.IO.Path.GetDirectoryName (this.Filename);
-		
-			// Console.WriteLine ("storing: {0}", lastPath);
-			// FIXME: find a way to only set this once per-dialog
-			propertyService.SetProperty (LastPathProperty, lastPath);
+			propertyService.SetProperty (LastPathProperty, this.CurrentFolder);
 		}
 	}
 }
