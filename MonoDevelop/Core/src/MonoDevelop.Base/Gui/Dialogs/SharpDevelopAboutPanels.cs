ca1cfff88e3814310798b6e4f1991c1eb6674699
jluke <jluke@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2005-01-28 02:10:38 +0000

    remove some old code
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@2183 b5faff8f-5bce-0310-b493-a43f95cdeebf

--- MonoDevelop/Core/src/MonoDevelop.Base/Gui/Dialogs/SharpDevelopAboutPanels.cs
+++ MonoDevelop/Core/src/MonoDevelop.Base/Gui/Dialogs/SharpDevelopAboutPanels.cs
@@ -11,32 +11,26 @@ using System.Text;
 using System.Reflection;
 
 using Gtk;
-using MonoDevelop.Gui;
-using MonoDevelop.Core.Properties;
-using MonoDevelop.Core.Services;
-using MonoDevelop.Internal.Project;
 using MonoDevelop.Services;
 
 namespace MonoDevelop.Gui.Dialogs
 {
 	public class AboutMonoDevelopTabPage : VBox
 	{
-		Label versionLabel = new Label ();
-		Label sponsorLabel = new Label ();
-		Label licenseLabel = new Label ();
-		Label copyrightLabel = new Label ();
-		
 		public AboutMonoDevelopTabPage ()
 		{
-			Version v = Assembly.GetEntryAssembly().GetName().Version;
-			versionLabel.Markup = String.Format ("<b>{0}</b>\n    {1}", GettextCatalog.GetString("Version"), v.Major + "." + v.Minor);
+			Label versionLabel = new Label ();
+			Label licenseLabel = new Label ();
+			Label copyrightLabel = new Label ();
+
+			Version v = Assembly.GetEntryAssembly ().GetName ().Version;
+			versionLabel.Markup = String.Format ("<b>{0}</b>\n    {1}", GettextCatalog.GetString ("Version"), v.Major + "." + v.Minor);
 			HBox hboxVersion = new HBox ();
 			hboxVersion.PackStart (versionLabel, false, false, 5);
 			
 			HBox hboxLicense = new HBox ();
 			licenseLabel.Markup = String.Format ("<b>License</b>\n    {0}", GettextCatalog.GetString ("Released under the GNU General Public license."));
 			hboxLicense.PackStart (licenseLabel, false, false, 5);
-
 
 			HBox hboxCopyright = new HBox ();
 			copyrightLabel.Markup = "<b>Copyright</b>\n    (c) 2000-2003 by icsharpcode.net\n    (c) 2004-2005 by MonoDevelop contributors";
@@ -51,11 +45,6 @@ namespace MonoDevelop.Gui.Dialogs
 	
 	public class VersionInformationTabPage : VBox
 	{
-		private TreeView listView;
-		private Button button;
-		private TreeStore store;
-		private Clipboard clipboard;
-		
 		public VersionInformationTabPage ()
 		{
 			TreeView listView = new TreeView ();
@@ -64,66 +53,33 @@ namespace MonoDevelop.Gui.Dialogs
 			listView.AppendColumn (GettextCatalog.GetString ("Version"), new CellRendererText (), "text", 1);
 			listView.AppendColumn (GettextCatalog.GetString ("Path"), new CellRendererText (), "text", 2);
 			
-			FillListView ();
+			listView.Model = FillListView ();
 			ScrolledWindow sw = new ScrolledWindow ();
 			sw.Add (listView);
-			this.PackStart (sw);
-			
-			HBox hbox = new HBox (false, 0);
-			//button = new Button (Gtk.Stock.Copy);
-			//button.Clicked += new EventHandler(CopyButtonClick);
-			hbox.PackStart (new Label (), false, true, 3);
-			//hbox.PackStart (button, false, false, 3);
-			hbox.PackStart (new Label (), false, true, 3);
-			this.PackStart (hbox, false, false, 6);
-			
-			listView.Model = store;
+			this.PackStart (sw, true, true, 0);
 		}
 		
-		void FillListView()
+		ListStore FillListView()
 		{
-			store = new TreeStore (typeof (string), typeof (string), typeof (string));
+			ListStore store = new ListStore (typeof (string), typeof (string), typeof (string));
 			
-			foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies()) {
-				AssemblyName name = asm.GetName();
+			foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies ()) {
+				AssemblyName name = asm.GetName ();
 				
 				string loc;
 				
 				try {
 					loc = System.IO.Path.GetFullPath (asm.Location);
-				} catch (Exception) {
+				} catch {
 					loc = GettextCatalog.GetString ("dynamic");
 				}
 				
-				store.AppendValues (name.Name, name.Version.ToString(), loc);
+				store.AppendValues (name.Name, name.Version.ToString (), loc);
 			}
 			
 			store.SetSortColumnId (0, SortType.Ascending);
-		}
-		
-		void CopyButtonClick(object o, EventArgs args)
-		{
-			StringBuilder versionInfo = new StringBuilder();
-			foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies()) {
-				AssemblyName name = asm.GetName();
-				versionInfo.Append(name.Name);
-				versionInfo.Append(",");
-				versionInfo.Append(name.Version.ToString());
-				versionInfo.Append(",");
-				try {
-					versionInfo.Append(System.IO.Path.GetFullPath (asm.Location));
-				} catch (Exception) {
-					versionInfo.Append(GettextCatalog.GetString ("dynamic"));
-				}
-				
-				versionInfo.Append(Environment.NewLine);
-			}
-			
-			// set to both the X and normal clipboards
-			clipboard = Clipboard.Get (Gdk.Atom.Intern ("CLIPBOARD", false));
-			clipboard.SetText (versionInfo.ToString ());
-			clipboard = Clipboard.Get (Gdk.Atom.Intern ("PRIMARY", false));
-			clipboard.SetText (versionInfo.ToString ());
+			return store;
 		}
 	}
 }
+
