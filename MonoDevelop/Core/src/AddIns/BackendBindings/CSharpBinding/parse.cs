5359ccd2f1d22eaec2cb8e5341cd2bd55511f909
jluke <jluke@b5faff8f-5bce-0310-b493-a43f95cdeebf>
2005-03-26 05:50:18 +0000

    improve my little test program
    git-svn-id: svn+ssh://mono-cvs.ximian.com/svn/monodevelop/trunk@2392 b5faff8f-5bce-0310-b493-a43f95cdeebf

--- MonoDevelop/Core/src/AddIns/BackendBindings/CSharpBinding/parse.cs
+++ MonoDevelop/Core/src/AddIns/BackendBindings/CSharpBinding/parse.cs
@@ -4,43 +4,63 @@ using System.IO;
 using ICSharpCode.SharpRefactory.Parser;
 using CSharpBinding.Parser;
 
-// recurse a dir for .cs files
-// and print out parsing errors
-class parse
+// print out parsing errors
+// from a dir recursively
+// or a single file
+class ParserTest
 {
-	static void Main (string[] args)
+	static Parser p = new Parser ();
+	static Lexer lexer;
+	static int counter = 0;
+	static int errors = 0;
+
+	static int Main (string[] args)
 	{
 		if (args.Length == 1 && Directory.Exists (args[0]))
-			new parse (args[0]);	
+			Parse (new DirectoryInfo (args[0]));
+		else if (args.Length == 1 && File.Exists (args[0]))
+			Parse (new FileInfo (args[0]));
 		else
-			PrintUsage ();
+			return PrintUsage ();
+
+		Console.WriteLine ("{0} out of {1} failed to parse correctly", errors, counter);
+		return 0;
 	}
 
-	static void PrintUsage ()
+	static int PrintUsage ()
 	{
 		Console.WriteLine ("usage: parse.exe <dir>");
+		return 0;
 	}
 
-	parse (string dir)
+	static void Parse (FileInfo file)
 	{
-		string[] files = Directory.GetFiles (dir, "*.cs");
-		if (files.Length < 1)
-			return;
-
-		Parser p = new Parser ();
-		Lexer lexer;
-
-		foreach (string f in files) {
-			lexer = new Lexer (new FileReader (f));
+		if (file.Exists) {
+			lexer = new Lexer (new FileReader (file.FullName));
 			p.Parse (lexer);
 			CSharpVisitor v = new CSharpVisitor ();
 			v.Visit (p.compilationUnit, null);
 			v.Cu.ErrorsDuringCompile = p.Errors.count > 0;
-			if (v.Cu.ErrorsDuringCompile)
-				Console.WriteLine ("errors in parsing " + f);
+			if (v.Cu.ErrorsDuringCompile) {
+				Console.WriteLine ("errors in parsing " + file.FullName);
+				errors ++;
+			}
+			else {
+				counter ++;
+			}
+
 			foreach (ErrorInfo error in p.Errors.ErrorInformation)
 				Console.WriteLine (error.ToString ());
 		}
+	}
+
+	static void Parse (DirectoryInfo dir)
+	{
+		foreach (FileInfo f in dir.GetFiles ("*.cs"))
+			Parse (f);
+
+		foreach (DirectoryInfo di in dir.GetDirectories ())
+			Parse (di);
 	}
 }
 
